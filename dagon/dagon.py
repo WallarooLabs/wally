f9079fd509e4b586c64b4c8f1c5f506c20620068
John Mumm <jtfmumm@gmail.com>
2016-01-15 16:31:39 -0500

    Almost working... just need to stop duplicating buffy nodes

--- dagon/dagon.py
+++ dagon/dagon.py
@@ -25,25 +25,26 @@ for section in parser.sections():
 edges = []
 origins = parser.options("edges")
 for origin in origins:
-    targets_raw = parser.get("edges", origin)
-    targets = targets_raw.split(",")
-    edges.append((origin, targets))
+    target = parser.get("edges", origin)
+    edges.append((origin, target))
 
 # Set up nodes
 ## This won't work for non-pipeline topologies until worker.py can have multiple outputs
-for origin,targets in edges:
+for origin,target in edges:
     action = nodes[origin]["d"]
     o_in_ip = nodes[origin]["in_ip"]
     o_out_ip = nodes[origin]["out_ip"]
-    subprocess.Popen(["python3.5", "../stream-py/MQ_udp.py", o_in_ip])
-    subprocess.Popen(["python3.5", "../stream-py/worker.py", o_in_ip, o_out_ip])
-
-    for target in targets:
-        t_in_ip = nodes[target]["in_ip"]
-        t_out_ip = nodes[target]["out_ip"]
-        subprocess.Popen(["../spike/spike", o_out_ip, t_in_ip, action, str(seed)])
-        subprocess.Popen(["python3.5", "../stream-py/MQ_udp.py", t_in_ip])
-        subprocess.Popen(["python3.5", "../stream-py/worker.py", t_in_ip, t_out_ip])
+    subprocess.Popen(["python3.5", "../buffy/MQ_udp.py", o_in_ip])
+    time.sleep(1)
+    subprocess.Popen(["python3.5", "../buffy/worker.py", o_in_ip, o_out_ip])
+    time.sleep(1)
+    t_in_ip = nodes[target]["in_ip"]
+    t_out_ip = nodes[target]["out_ip"]
+    subprocess.Popen(["../spike/spike", o_out_ip, t_in_ip, action, str(seed)])
+    subprocess.Popen(["python3.5", "../buffy/MQ_udp.py", t_in_ip])
+    time.sleep(1)
+    subprocess.Popen(["python3.5", "../buffy/worker.py", t_in_ip, t_out_ip])
+    time.sleep(1)
 
 source_addr = nodes[edges[0][0]]["in_ip"].split(":")
 sink_addr = nodes[edges[len(edges) - 1][1]]["out_ip"].split(":")
