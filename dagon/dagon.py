a4f7c2df83802c0612cbf27ff06bc53a1050399f
John Mumm <jtfmumm@gmail.com>
2016-01-15 15:45:00 -0500

    Working pipeline topologies

--- dagon/dagon.py
+++ dagon/dagon.py
@@ -37,14 +37,14 @@ for origin,target in edges:
     o_out_ip = nodes[origin]["out_ip"]
     t_in_ip = nodes[target]["in_ip"]
     t_out_ip = nodes[target]["out_ip"]
-    subprocess.call(["python3.5", "MQ_udp.py", o_in_ip])
-    subprocess.call(["python3.5", "worker.py", o_in_ip, o_out_ip])
-    subprocess.call(["./spike", o_out_ip, t_in_ip, action, str(seed)])
-    subprocess.call(["python3.5", "MQ_udp.py", t_in_ip])
-    subprocess.call(["python3.5", "worker.py", t_in_ip, t_out_ip])
+    subprocess.Popen(["python3.5", "../stream-py/MQ_udp.py", o_in_ip])
+    subprocess.Popen(["python3.5", "../stream-py/worker.py", o_in_ip, o_out_ip])
+    subprocess.Popen(["../spike/spike", o_out_ip, t_in_ip, action, str(seed)])
+    subprocess.Popen(["python3.5", "../stream-py/MQ_udp.py", t_in_ip])
+    subprocess.Popen(["python3.5", "../stream-py/worker.py", t_in_ip, t_out_ip])
 
-source_addr = nodes[edges[0]]["in_ip"].split(":")
-sink_addr = nodes[edges[len(edges) - 1]]["out_ip"].split(":")
+source_addr = nodes[edges[0][0]]["in_ip"].split(":")
+sink_addr = nodes[edges[len(edges) - 1][1]]["out_ip"].split(":")
 
 # Set up testing framework
-subprocess.call(["./giles", source_addr[0], source_addr[1], sink_addr[0], sink_addr[1]])
+subprocess.call(["../giles/giles", source_addr[0], source_addr[1], sink_addr[0], sink_addr[1]])
