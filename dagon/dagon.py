b6dd44d1b5f2967a81ec638d3929c3cc22287e33
John Mumm <jtfmumm@gmail.com>
2016-01-19 17:53:04 -0500

    Add Click command line parsing

--- dagon/dagon.py
+++ dagon/dagon.py
@@ -1,3 +1,4 @@
+import click
 import sys
 import os
 import time
@@ -7,6 +8,10 @@ import signal
 from configparser import SafeConfigParser
 
 PAUSE = 1
+CONFIG_FILENAME = ""
+DURATION = 0
+SEED = 0
+INFINITE = False
 
 def print_buffy_node(in_ip, out_ip):
     print("dagon: Creating BUFFY node " + in_ip + " --> " + out_ip)
@@ -21,108 +26,110 @@ def print_instructions_and_exit():
 ## CONFIGURE
 
 # Parse command line args
-if (sys.argv[1] == "help"):
-    print_instructions_and_exit()
+@click.command()
+@click.argument("config_file")
+@click.argument("duration")
+@click.option("--seed", default=int(round(time.time() * 1000)), help="Random number seed")
+@click.option("--infinite", is_flag=True)
+def cli(config_file, duration, seed, infinite):
+    CONFIG_FILENAME = config_file + ".ini"
+    DURATION = int(duration)
+    SEED = seed
+    INFINITE = infinite
 
-try:
-    config_filename = sys.argv[1] + ".ini"
-    duration = int(sys.argv[2])
-    if (len(sys.argv) > 3):
-        seed = sys.argv[3]
-    else:
-        seed = int(round(time.time() * 1000))
-except:
-    print_instructions_and_exit()
+    # Get config info
+    parser = SafeConfigParser()
+    parser.read(CONFIG_FILENAME)
 
-# Get config info
-parser = SafeConfigParser()
-parser.read(config_filename)
+    nodes = {}
+    for section in parser.sections():
+        if section == "edges": continue
+        nodes[section] = {}
+        options = parser.options(section)
+        for option in options:
+            nodes[section][option] = parser.get(section, option)
 
-nodes = {}
-for section in parser.sections():
-    if section == "edges": continue
-    nodes[section] = {}
-    options = parser.options(section)
-    for option in options:
-        nodes[section][option] = parser.get(section, option)
-
-edges = []
-origins = parser.options("edges")
-for origin in origins:
-    target = parser.get("edges", origin)
-    edges.append((origin, target))
+    edges = []
+    origins = parser.options("edges")
+    for origin in origins:
+        target = parser.get("edges", origin)
+        edges.append((origin, target))
 
 
-## RUN TOPOLOGY
+    ## RUN TOPOLOGY
 
-processes = [] # A list of spawned subprocesses
-devnull = open(os.devnull, "w") # For suppressing stdout/stderr of subprocesses
-giles_output = open("dagon.giles", "w")
-giles_output.seek(0)
-giles_output.truncate()
-print("dagon: Creating topology...")
+    processes = [] # A list of spawned subprocesses
+    devnull = open(os.devnull, "w") # For suppressing stdout/stderr of subprocesses
+    giles_output = open("dagon.giles", "w")
+    giles_output.seek(0)
+    giles_output.truncate()
+    print("dagon: Creating topology...")
 
-# Set up origin
-origin_node = edges[0][0]
-origin_in_ip = nodes[origin_node]["in_ip"]
-origin_out_ip = nodes[origin_node]["out_ip"]
-print_buffy_node(origin_in_ip, origin_out_ip)
-processes.append(subprocess.Popen(["python3.5", "../buffy/MQ_udp.py", origin_in_ip], stdout=devnull, stderr=devnull))
-time.sleep(PAUSE)
-processes.append(subprocess.Popen(["python3.5", "../buffy/worker.py", origin_in_ip, origin_out_ip], stdout=devnull, stderr=devnull))
-time.sleep(PAUSE)
-
-# Set up targets
-for f,t in edges:
-    action = nodes[f]["d"]
-    f_in_ip = nodes[f]["in_ip"]
-    f_out_ip = nodes[f]["out_ip"]
-    t_in_ip = nodes[t]["in_ip"]
-    t_out_ip = nodes[t]["out_ip"]
-    print_spike_node(f_out_ip, t_in_ip, action)
-    processes.append(subprocess.Popen(["../spike/spike", f_out_ip, t_in_ip, action, str(seed)], stdout=devnull, stderr=devnull))
-    print_buffy_node(t_in_ip, t_out_ip)
-    processes.append(subprocess.Popen(["python3.5", "../buffy/MQ_udp.py", t_in_ip], stdout=devnull, stderr=devnull))
+    # Set up origin
+    origin_node = edges[0][0]
+    origin_in_ip = nodes[origin_node]["in_ip"]
+    origin_out_ip = nodes[origin_node]["out_ip"]
+    print_buffy_node(origin_in_ip, origin_out_ip)
+    processes.append(subprocess.Popen(["python3.5", "../buffy/MQ_udp.py", origin_in_ip], stdout=devnull, stderr=devnull))
     time.sleep(PAUSE)
-    processes.append(subprocess.Popen(["python3.5", "../buffy/worker.py", t_in_ip, t_out_ip], stdout=devnull, stderr=devnull))
+    processes.append(subprocess.Popen(["python3.5", "../buffy/worker.py", origin_in_ip, origin_out_ip], stdout=devnull, stderr=devnull))
     time.sleep(PAUSE)
 
-source_addr = nodes[edges[0][0]]["in_ip"].split(":")
-sink_addr = nodes[edges[len(edges) - 1][1]]["out_ip"].split(":")
-print("dagon: Source is " + source_addr[0] + ":" + source_addr[1])
-print("dagon: Sink is " + sink_addr[0] + ":" + sink_addr[1])
+    # Set up targets
+    for f,t in edges:
+        action = nodes[f]["d"]
+        f_in_ip = nodes[f]["in_ip"]
+        f_out_ip = nodes[f]["out_ip"]
+        t_in_ip = nodes[t]["in_ip"]
+        t_out_ip = nodes[t]["out_ip"]
+        print_spike_node(f_out_ip, t_in_ip, action)
+        processes.append(subprocess.Popen(["../spike/spike", f_out_ip, t_in_ip, action, str(SEED)], stdout=devnull, stderr=devnull))
+        print_buffy_node(t_in_ip, t_out_ip)
+        processes.append(subprocess.Popen(["python3.5", "../buffy/MQ_udp.py", t_in_ip], stdout=devnull, stderr=devnull))
+        time.sleep(PAUSE)
+        processes.append(subprocess.Popen(["python3.5", "../buffy/worker.py", t_in_ip, t_out_ip], stdout=devnull, stderr=devnull))
+        time.sleep(PAUSE)
 
-# Set up testing framework
-print("dagon: Creating GILES node writing to source and listening at sink")
-processes.append(subprocess.Popen(["../giles/giles", source_addr[0], source_addr[1], sink_addr[0], sink_addr[1]], stdout=giles_output, stderr=giles_output))
-print("dagon: Test is running...")
+    source_addr = nodes[edges[0][0]]["in_ip"].split(":")
+    sink_addr = nodes[edges[len(edges) - 1][1]]["out_ip"].split(":")
+    print("dagon: Source is " + source_addr[0] + ":" + source_addr[1])
+    print("dagon: Sink is " + sink_addr[0] + ":" + sink_addr[1])
 
-# Let test run for duration
-time.sleep(duration)
-print("dagon: Finished")
+    # Set up testing framework
+    print("dagon: Creating GILES node writing to source and listening at sink")
+    processes.append(subprocess.Popen(["../giles/giles", source_addr[0], source_addr[1], sink_addr[0], sink_addr[1]], stdout=giles_output, stderr=giles_output))
+    print("dagon: Test is running...")
 
-# Kill subprocesses
-for process in processes:
-    os.kill(process.pid, signal.SIGTERM)
+    # Let test run for duration
+    time.sleep(DURATION)
+    print("dagon: Finished")
 
-
-## CALCULATE TEST RESULTS
-
-badline_count = 0
-giles_output.close()
-giles_output = open("dagon.giles", "r")
-for line in giles_output.readlines():
-    if re.search("bad message", line):
-        badline_count += 1
-
-test_result = "PASSED" if badline_count == 0 else "FAILED"
-print("\ndagon: Test has " + test_result)
-if test_result == "FAILED":
-    print("dagon: Bad messages = " + str(badline_count))
+    # Kill subprocesses
+    for process in processes:
+        os.kill(process.pid, signal.SIGTERM)
 
 
-## CLEAN UP
+    ## CALCULATE TEST RESULTS
 
-giles_output.close()
-devnull.close()
-sys.exit()
+    badline_count = 0
+    giles_output.close()
+    giles_output = open("dagon.giles", "r")
+    for line in giles_output.readlines():
+        if re.search("bad message", line):
+            badline_count += 1
+
+    test_result = "PASSED" if badline_count == 0 else "FAILED"
+    print("\ndagon: Test has " + test_result)
+    if test_result == "FAILED":
+        print("dagon: Bad messages = " + str(badline_count))
+
+
+    ## CLEAN UP
+
+    giles_output.close()
+    devnull.close()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    cli()
