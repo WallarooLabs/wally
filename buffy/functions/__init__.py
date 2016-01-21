19fbe11427d51729ba8d6d66915aff6303beb220
Nisan Haramati <hanisan@gmail.com>
2016-01-21 15:42:04 -0800

    Fetch on lowercase and record proper name

--- buffy/functions/__init__.py
+++ buffy/functions/__init__.py
@@ -4,13 +4,13 @@ __FUNCS__ = {}
 
 
 def get_function(func_name):
-    return __FUNCS__[func_name]
+    return __FUNCS__[func_name.lower()]
 
 # Import each function manually
 # TODO: automate this part so function files are drop-in compatible
 from .pong import FUNC_NAME, func
-__FUNCS__[FUNC_NAME.lower()] = func
+__FUNCS__[FUNC_NAME.lower()] = (func, FUNC_NAME)
 
 from .passthrough import FUNC_NAME, func
-__FUNCS__[FUNC_NAME] = func
+__FUNCS__[FUNC_NAME.lower()] = (func, FUNC_NAME)
 
