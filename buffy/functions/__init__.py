10141536a06a895d02e097678046309c54cf10fd
Nisan Haramati <hanisan@gmail.com>
2016-01-21 15:20:52 -0800

    Use lowercase in get_func's dict

--- buffy/functions/__init__.py
+++ buffy/functions/__init__.py
@@ -9,7 +9,7 @@ def get_function(func_name):
 # Import each function manually
 # TODO: automate this part so function files are drop-in compatible
 from .pong import FUNC_NAME, func
-__FUNCS__[FUNC_NAME] = func
+__FUNCS__[FUNC_NAME.lower()] = func
 
 from .passthrough import FUNC_NAME, func
 __FUNCS__[FUNC_NAME] = func
