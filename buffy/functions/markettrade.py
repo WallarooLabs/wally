b7143bf4fb26f0b63cb4e8e30766f81743c63ba5
Nisan Haramati <hanisan@gmail.com>
2016-02-08 12:16:58 -0800

    Refactor and remove dangling code
    Skip 'fill' and 'heartbeat' type messages.
    Pass 'order' type messages to a process_order() function.
    Pass 'nbbo' type messages to a process_nbbo() function.

--- buffy/functions/markettrade.py
+++ buffy/functions/markettrade.py
@@ -18,46 +18,40 @@ FUNC_NAME = 'Markettrade'
 def func(input):
     # Deserialize input
     msg = json.loads(input)
-    if msg['stream_type'] == 'nbbo':
-        # Update market info in memory
-        state.get_attribute('market', {})[msg['symbol']] = {
-            'id': msg['id'],
-            'last_msg_time': msg['time'],
-            'symbol': msg['symbol'],
-            'bid': msg['bid'],
-            'offer': msg['offer'],
-            'mid': (msg['bid'] + msg['offer'])/2,
-            'stop_new_orders': ((msg['offer'] - msg['bid']) >= 0.05 and
-                                (msg['offer'] - msg['bid']) >= 0.05)}
+    if msg['type'] == 'nbbo':
+        process_order(msg)
+    elif msg['type'] == 'order':
+        process_order(msg)
+    elif msg['type'] in ('fill', 'heartbeat'):
+        return None
 
-    elif msg['stream_type'] == 'order':
-        # Orders
-        if msg['type'] == 'order':
-            # Reject order if: order already exists, the symbol has
-            # stop_new_orders set to True or ???
-            if (msg['order'] in state.get_attribute('orders', {}) or
-                state.get_attribute('market',
-                                    {})[msg['symbol']]['stop_new_orders']):
-                return reject_order(msg)
 
-            # otherwise, accept the order
-            return accept_order(msg)
+def process_nbbo(msg):
+    # Update market info in memory
+    state.get_attribute('market', {})[msg['symbol']] = {
+        'id': msg['id'],
+        'last_msg_time': msg['time'],
+        'symbol': msg['symbol'],
+        'bid': msg['bid'],
+        'offer': msg['offer'],
+        'mid': (msg['bid'] + msg['offer'])/2,
+        'stop_new_orders': ((msg['offer'] - msg['bid']) >= 0.05 and
+                            (msg['offer'] - msg['bid']) >= 0.05)}
+    return None
 
-        # Fills
-        elif msg['type'] == 'fill':
-            order = state.get_attribute('order', {})
-            if msg['order'] not in order:
-                return reject_trade(msg)
 
-            # check if the quantity in the fill overflows the order target
-            if ((order[msg['order']]['quantity'] -
-                 order[msg['order']]['filled']) > msg['quantity']):
-                return reject_trade(msg)
+def process_order(msg):
+    # Orders
+    if msg['type'] == 'order':
+        # Reject order if: order already exists, the symbol has
+        # stop_new_orders set to True or ???
+        if (msg['order'] in state.get_attribute('orders', {}) or
+            state.get_attribute('market',
+                                {})[msg['symbol']]['stop_new_orders']):
+            return reject_order(msg)
 
-            # process trade
-            mult = 1 if order[msg['order']]['side'] == 'buy' else -1
-            order[msg['order']]['filled'] += mult*msg['quantity']
-            return accept_trade(msg)
+        # otherwise, accept the order
+        return accept_order(msg)
 
 
 def reject_order(msg):
@@ -65,12 +59,4 @@ def reject_order(msg):
 
 
 def accept_order(msg):
-    pass
-
-
-def reject_trade(msg):
-    pass
-
-
-def accept_trade(msg):
     pass
