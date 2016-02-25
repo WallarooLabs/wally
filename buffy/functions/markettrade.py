3ed4bd0b66744c6b0ec8520a85ab6e130b79863d
Nisan Haramati <hanisan@gmail.com>
2016-02-25 15:21:35 -0800

    Add parse_fix function

--- buffy/functions/markettrade.py
+++ buffy/functions/markettrade.py
@@ -16,13 +16,14 @@ FUNC_NAME = 'Marketspread'
 
 def func(input):
     # Deserialize input
-    msg = json.loads(input)
+    msg = parse_fix(input)
     if msg['type'] == 'nbbo':
         process_order(msg)
     elif msg['type'] == 'order':
         process_order(msg)
     elif msg['type'] in ('fill', 'heartbeat'):
         return None
+
 
 
 def process_nbbo(msg):
@@ -59,3 +60,34 @@ def reject_order(msg):
 
 def accept_order(msg):
     pass
+
+
+SOH = '\x01'
+TAGS = {'0': ('message_id', str),
+        '1': ('client_id', str),
+        '11': ('order_id', str),
+        '38': ('order_qty', int),
+        '44': ('price', float),
+        '54': ('side', int),
+        '55': ('symbol', str),
+        '60': ('sent_time', str),
+        '132': ('bid', float),
+        '133': ('offer', float),}
+
+
+def parse_fix(input):
+    tuples = [part.split('=') for part in input.split(SOH)]
+    output = {TAGS[tup[0]][0]: TAGS[tup[0]][1](tup[1]) for tup in tuples
+              if tup[0] in TAGS}
+    return output
+
+
+# TESTS #
+def test_parse_fix():
+    input = ('8=FIX.4.2\x019=121\x0135=D\x011=CLIENT35\x0111=s0XCIa\x01'
+             '21=3\x0138=4000\x0140=2\x0144=252.85366153511416\x0154=1\x01'
+             '55=TSLA\x0160=20151204-14:30:00.000\x01107=Tesla Motors\x01'
+             '10=108\x01')
+    output = parse_fix(input)
+    print(output)
+    assert(0)
