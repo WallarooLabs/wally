f8e6676e85a34afcdf64dfcb783d6ce0d220a241
Nisan Haramati <hanisan@gmail.com>
2016-02-29 17:30:37 -0800

    Update parse_fix and tests
    - Add information required to parse NBBO tags
    - Add additional general FIX tags
    - Split test_parse_fix to test_parse_fix_trade and test_parse_fix_nbbo
    - Update expected output for both given new tags

--- buffy/functions/marketspread.py
+++ buffy/functions/marketspread.py
@@ -81,21 +81,28 @@ SOH = '\x01'
 # See http://www.onixs.biz/fix-dictionary/4.2/fields_by_tag.html
 # for complete tag definitions
 MSG_TYPES = {'0': 'heartbeat',
-             'D': 'order',}
+             'D': 'order',
+             'S': 'nbbo',}
 SIDES = {'1': 'buy',
          '2': 'sell',}
 TAGS = {'0': ('MessageId', str),
         '1': ('Account', str),
+        '8': ('BeginString', str),
+        '9': ('BodyLength', int),
+        '10': ('CheckSum', str),
         '11': ('OrderId', str),
+        '21': ('HandlInst', str),
         '35': ('MsgType', lambda v: MSG_TYPES.get(v, None)),
         '37': ('OrderId', str),
         '38': ('OrderQty', float),
         '39': ('OrdStatus', str),
+        '40': ('OrdType', str),
         '44': ('Price', float),
         '54': ('Side', lambda v: SIDES.get(v, None)),
         '55': ('Symbol', str),
         '60': ('TransactTime', str),
         '107': ('SecurityDesc', str),
+        '117': ('QuoteId', str),
         '132': ('BidPx', float),
         '133': ('OfferPx', float),}
 
@@ -108,17 +115,30 @@ def parse_fix(input):
 
 
 # TESTS #
-def test_parse_fix():
+def test_parse_fix_trade():
     input = ('8=FIX.4.2\x019=121\x0135=D\x011=CLIENT35\x0111=s0XCIa\x01'
              '21=3\x0138=4000\x0140=2\x0144=252.85366153511416\x0154=1\x01'
              '55=TSLA\x0160=20151204-14:30:00.000\x01107=Tesla Motors\x01'
              '10=108\x01')
-    expected = {'Account': 'CLIENT35', 'OrderQty': 4000.0, 'Symbol': 'TSLA',
-                'Price': 252.85366153511416, 'OrderId': 's0XCIa',
-                'TransactTime': '20151204-14:30:00.000',
-                'SecurityDesc': 'Tesla Motors', 'Side': 'buy',
-                'MsgType': 'order'}
+    expected = {'Side': 'buy', 'TransactTime': '20151204-14:30:00.000',
+                'Account': 'CLIENT35', 'MsgType': 'order', 'BodyLength': 121,
+                'OrderId': 's0XCIa', 'BeginString': 'FIX.4.2',
+                'SecurityDesc': 'Tesla Motors', 'Symbol': 'TSLA',
+                'CheckSum': '108', 'OrderQty': 4000.0, 'HandlInst': '3',
+                'Price': 252.85366153511416, 'OrdType': '2'}
 
+    output = parse_fix(input)
+    assert(output == expected)
+
+
+def test_parse_fix_nbbo():
+    input = ('8=FIX.4.2\x019=64\x0135=S\x0155=WETF\x01'
+             '60=20151204-14:30:00.000\x01117=S\x01132=16.64\x01133=16.64'
+             '\x0110=098\x01')
+    expected = {'MsgType': 'nbbo', 'QuoteId': 'S', 'BodyLength': 64,
+                'BidPx': 16.64, 'OfferPx': 16.64, 'Symbol': 'WETF',
+                'BeginString': 'FIX.4.2', 'CheckSum': '098',
+                'TransactTime': '20151204-14:30:00.000'}
     output = parse_fix(input)
     assert(output == expected)
 
