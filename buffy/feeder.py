8998e0f1f629c1f5c7111986109fe89d5f8db5b5
Nisan Haramati <hanisan@gmail.com>
2016-03-29 14:12:38 -0700

    Fix how FeedEpoch is appended

--- buffy/feeder.py
+++ buffy/feeder.py
@@ -52,7 +52,9 @@ def feed(address, delay, path, console_log, file_log, log_level):
     c = 0
     while sources:
         source, source_size = sources[c % len(sources)]
-        msg = source.readline().decode() + '999={}\x01'.format(time.time())
+        msg = source.readline().decode().strip()
+        msg = '"{}\x01999={}\x01"'.format(msg.strip('"').strip('\x01'),
+                                          time.time())
         if not msg and source.tell() >= source_size:
             logger.info('Removing file %s' % source.name)
             sources.remove((source, source_size))
