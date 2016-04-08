c5ba490b9827fc9799814625aeb7a852b20c81a6
Nisan Haramati <hanisan@gmail.com>
2016-04-08 15:31:06 -0700

    Sample output doubler

---
+++ buffy/functions/inputdoubler.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python3
+
+"""
+InputDoubler
+
+Double the input into two outputs
+"""
+
+FUNC_NAME = 'InputDoubler'
+
+
+def func(input):
+    yield (0, input)
+    yield (1, input)
