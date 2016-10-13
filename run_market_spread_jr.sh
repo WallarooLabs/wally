f05929748c94640374b8a6871e865db7ad51aae0
Alan Mosca <alan@sendence.com>
2016-10-13 12:27:45 +0100

    Make the market-spread-jr script more useful, by: - using the MESSAGES env variable to change the number of messages in the run - using the DOWNLOAD env variable to skip downloading the UI - being more aggressive at killing the processes running inside screen

--- run_market_spread_jr.sh
+++ run_market_spread_jr.sh
@@ -3,20 +3,27 @@ set fail -o -e
 
 TMPDIR=/tmp/market-spread-jr-run/
 SCREEN_SESSION_NAME="RUNNING_MARKET_SPREAD_JR"
-DOWNLOAD=0
+
+if [ -z "$MESSAGES" ]; then
+  MESSAGES=150000000
+fi  
+
+if [ -z "$DOWNLOAD" ]; then
+  DOWNLOAD=1
+fi  
 
 mkdir -p $TMPDIR/metrics_reporter_ui
 mkdir -p $TMPDIR/market_spread_reports_ui
-wget -N https://s3.amazonaws.com/sendence-dev/wallaroo/ui-bins/market_spread_reports_ui.tar.gz -O $TMPDIR/market_spread_reports_ui.tar.gz
-wget -N https://s3.amazonaws.com/sendence-dev/wallaroo/ui-bins/metrics_reporter_ui.tar.gz -O $TMPDIR/metrics_reporter_ui.tar.gz
+if [ "$DOWNLOAD"  == "1" ]; then
+  wget -N https://s3.amazonaws.com/sendence-dev/wallaroo/ui-bins/market_spread_reports_ui.tar.gz -O $TMPDIR/market_spread_reports_ui.tar.gz
+  wget -N https://s3.amazonaws.com/sendence-dev/wallaroo/ui-bins/metrics_reporter_ui.tar.gz -O $TMPDIR/metrics_reporter_ui.tar.gz
+fi  
 
 tar zxf $TMPDIR/market_spread_reports_ui.tar.gz -C $TMPDIR/market_spread_reports_ui
 tar zxf $TMPDIR/metrics_reporter_ui.tar.gz -C $TMPDIR/metrics_reporter_ui
 
 make build-apps-market-spread-jr
 make build-giles-sender
-
-screen -dmS $SCREEN_SESSION_NAME
 
 pushd $TMPDIR/metrics_reporter_ui
 until [ exec 6<>/dev/tcp/127.0.0.1/4000 ]; do
@@ -30,15 +37,17 @@ until [ exec 6<>/dev/tcp/127.0.0.1/4001 ]; do
 done
 popd
 
-screen -S $SCREEN_SESSION_NAME -p 0 -X stuff "apps/market-spread-jr/market-spread-jr -i 127.0.0.1:7000,127.0.0.1:7001 -o 127.0.0.1:5555 -m 127.0.0.1:5001 -c 127.0.0.1:6000 -d 127.0.0.1:6001 -f ../../demos/marketspread/initial-nbbo-fixish.msg -e 150000000 -n node-name
+screen -dmS $SCREEN_SESSION_NAME
+
+screen -S $SCREEN_SESSION_NAME -p 0 -X stuff "apps/market-spread-jr/market-spread-jr -i 127.0.0.1:7000,127.0.0.1:7001 -o 127.0.0.1:5555 -m 127.0.0.1:5001 -c 127.0.0.1:6000 -d 127.0.0.1:6001 -f ../../demos/marketspread/initial-nbbo-fixish.msg -e $MESSAGES -n node-name
 "
 
-screen -S $SCREEN_SESSION_NAME -X screen 1
-screen -S $SCREEN_SESSION_NAME -p 1 -X stuff "giles/sender/sender -b 127.0.0.1:7000 -m 100000000 -s 300 -i 2_500_000 -f demos/marketspread/350k-nbbo-fixish.msg -r --ponythreads=1 -y -g 46 -w
+screen -S $SCREEN_SESSION_NAME -X screen 0
+screen -S $SCREEN_SESSION_NAME -p 1 -X stuff "giles/sender/sender -b 127.0.0.1:7000 -m $(( 1 + $MESSAGES * 2 / 3)) -s 300 -i 2_500_000 -f demos/marketspread/350k-nbbo-fixish.msg -r --ponythreads=1 -y -g 46 -w
 "
 
 screen -S $SCREEN_SESSION_NAME -X screen 2
-screen -S $SCREEN_SESSION_NAME -p 2 -X stuff "giles/sender/sender -b 127.0.0.1:7001 -m 50000000 -s 300 -i 5_000_000 -f demos/marketspread/350k-orders-fixish.msg -r --ponythreads=1 -y -g 57 -w
+screen -S $SCREEN_SESSION_NAME -p 2 -X stuff "giles/sender/sender -b 127.0.0.1:7001 -m $(( 1 + $MESSAGES / 3)) -s 300 -i 5_000_000 -f demos/marketspread/350k-orders-fixish.msg -r --ponythreads=1 -y -g 57 -w
 "
 
 read -n1 -r -p "run \`screen -r $SCREEN_SESSION_NAME\` to see what's happening, and press any key when you want to kill everything" key
@@ -49,4 +58,8 @@ cd $TMPDIR/market_spread_reports_ui
 bin/market_spread_reports_ui stop
 popd
 
+screen -S $SCREEN_SESSION_NAME -p 0 -X stuff ^C
+screen -S $SCREEN_SESSION_NAME -p 1 -X stuff ^C
+screen -S $SCREEN_SESSION_NAME -p 2 -X stuff ^C
+sleep 5
 screen -X -S $SCREEN_SESSION_NAME quit
