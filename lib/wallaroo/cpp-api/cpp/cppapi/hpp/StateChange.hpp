334297f1e66077cea9be580244ffa81c758763e6
Kevin Goldstein <kevin@sendence.com>
2016-11-11 14:48:06 -0500

    convienience functions & fields

--- lib/wallaroo/cpp-api/cpp/cppapi/hpp/StateChange.hpp
+++ lib/wallaroo/cpp-api/cpp/cppapi/hpp/StateChange.hpp
@@ -10,17 +10,25 @@ namespace wallaroo
 {
 class StateChange: public ManagedObject
 {
-public:
-  virtual const char *name() = 0;
-  virtual void apply(State *state_) = 0;
-  virtual void to_log_entry(char *bytes_) = 0;
-  virtual uint64_t id() = 0;
-  virtual size_t get_log_entry_size() = 0;
-  virtual size_t get_log_entry_size_header_size() = 0;
-  virtual size_t read_log_entry_size_header(char *bytes_) = 0;
-  virtual bool read_log_entry(char *bytes_) = 0;
+  protected:
+    uint64_t _id;
 
-  virtual string str(){ return "StateChange"; };
+  public:
+    StateChange() : _id(0) {}
+    StateChange(uint64_t id_) : _id(id_) {}
+
+  public:
+    virtual const char *name() = 0;
+    virtual void apply(State *state_) = 0;
+    virtual void to_log_entry(char *bytes_) = 0;
+    virtual size_t get_log_entry_size() = 0;
+    virtual size_t get_log_entry_size_header_size() = 0;
+    virtual size_t read_log_entry_size_header(char *bytes_) = 0;
+    virtual bool read_log_entry(char *bytes_) = 0;
+
+  public:
+    virtual uint64_t id() { return _id; }
+    virtual string str() { return "StateChange"; }
 };
 }
 
