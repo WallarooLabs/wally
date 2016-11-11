d3935c633d4a3643f5d940c3bf7b0030ad0ab4f0
Kevin Goldstein <kevin@sendence.com>
2016-11-11 13:53:43 -0500

    added str()

--- lib/wallaroo/cpp-api/cpp/cppapi/hpp/StateChange.hpp
+++ lib/wallaroo/cpp-api/cpp/cppapi/hpp/StateChange.hpp
@@ -12,13 +12,15 @@ class StateChange: public ManagedObject
 {
 public:
   virtual const char *name() = 0;
-  virtual uint64_t id() = 0;
   virtual void apply(State *state_) = 0;
-  virtual size_t get_log_entry_size() = 0;
   virtual void to_log_entry(char *bytes_) = 0;
+  virtual uint64_t id() = 0;
+  virtual size_t get_log_entry_size() = 0;
   virtual size_t get_log_entry_size_header_size() = 0;
   virtual size_t read_log_entry_size_header(char *bytes_) = 0;
   virtual bool read_log_entry(char *bytes_) = 0;
+
+  virtual string str(){ return "StateChange"; };
 };
 }
 
