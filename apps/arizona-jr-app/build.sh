05b20c6cd7db6d063b7c63def510552fcc5f6846
goldsteink <kevin@sendence.com>
2016-12-01 17:08:38 -0500

    copies to multiple hosts specified in the hosts.txt

--- apps/arizona-jr-app/build.sh
+++ apps/arizona-jr-app/build.sh
@@ -39,3 +39,23 @@ echo $WALLAROO_HOME
 WALL_PATH=$WALLAROO_HOME:../../lib/wallaroo/cpp-api/cpp/cppapi/build/build/lib:./build/lib:/usr/lib
 echo "Wallaroo path: $WALL_PATH"
 ponyc --debug --path=$WALL_PATH --output=build arizona-jr-app/
+cp build/arizona-jr-app ~/dist/bin
+
+
+name=`uname`
+if [ "$name" != "Linux" ] ; then
+    echo "Not a Linux machine, not copying to different hosts"
+    exit 0
+fi
+
+echo "Copying to exec server(s).."
+hosts=`cat hosts.txt`
+for host in $hosts
+do
+    scp -i ~/.ssh/us-east-1.pem ~/dist/bin/arizona-jr-app ec2-user@$host:/apps/dev/arizona/bin/
+    if [ $? -gt 0 ] ; then
+	echo "$host: FAILED"
+    else
+	echo "$host: Done!"
+    fi
+done
