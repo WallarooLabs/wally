bdd982e443e86988010fbd22aa4a553386459a06
goldsteink <kevin@sendence.com>
2016-12-06 09:03:42 -0500

    added sender

--- apps/arizona-jr-app/distribute.sh
+++ apps/arizona-jr-app/distribute.sh
@@ -11,6 +11,7 @@ hosts=`cat hosts.txt`
 for host in $hosts
 do
     scp -i ~/.ssh/us-east-1.pem ~/dist/bin/arizona-jr-app ec2-user@$host:/apps/dev/arizona/bin/
+    scp -i ~/.ssh/us-east-1.pem ~/dist/bin/sender ec2-user@$host:/apps/dev/arizona/bin/
     if [ $? -gt 0 ] ; then
 	echo "$host: FAILED"
     else
