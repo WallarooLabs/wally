4b75abe90d9ac94acde2944c370e072eb4fe6486
Andrew Turley <aturley@acm.org>
2016-12-22 10:28:01 -0600

    Add C++ state object for arizona-source app
    The state object encapsulates the logic for querying and managing the
    state of Arizona.
    
    There are a few issues that should be noted:
    * Only handles state queries and updates related to adding order
    * If a client, account, or ISIN does not exist when an order is added,
      it will be added (this may not be desired behavior moving forward)
    * If a client, account, or ISIN does not exist when checking proceeds
      value that will occur after an order is added, then all of the
      proceeds values are assumed to be 0.
    * There is no optimization for the situation where there is only one
      client, so when getting client information there is always a map
      lookup. This may impact the performance in the case where state is
      partitioned by client across state actors.
    
    As more features are added, we will decide how to more appropriately
    handle these situations, but for now this gives us a starting place.

---
+++ apps/arizona-source-app/cpp/ArizonaStateTests.cpp
@@ -0,0 +1,174 @@
+#include "ArizonaState.hpp"
+
+bool test_proceeds()
+{
+  Proceeds p1(1.0, 2.0, 3.0, 4.0);
+  Proceeds p2(1.0, 2.0, 3.0, 4.0);
+
+  return p1 == p2;
+}
+
+bool test_proceeds_add()
+{
+  Proceeds p1(1.0, 2.0, 3.0, 4.0);
+  Proceeds p2(1.0, 2.0, 3.0, 4.0);
+  Proceeds p3(2.0, 4.0, 6.0, 8.0);
+
+  return p1.add(p2) == p3;
+}
+
+bool test_order()
+{
+  string order_id("o1");
+  Order x(order_id, Side::Buy, 10, 50.0);
+  Order y(order_id, Side::Buy, 10, 50.0);
+  return x == y;
+}
+
+bool test_orders()
+{
+  Orders orders;
+  string order_id1("o1");
+  string order_id2("o2");
+  orders.add_order(order_id1, Side::Buy, 10, 50.0);
+  orders.add_order(order_id2, Side::Buy, 20, 70.0);
+  Proceeds p1 = Proceeds(0.0, 1900.0, 0, 0.0);
+  Proceeds p2 = orders.proceeds();
+  return p1 == p2;
+}
+
+bool test_isin()
+{
+  string isin_id("i1");
+  ISIN isin(isin_id);
+  string order_id1("o1");
+  string order_id2("o2");
+  isin.add_order(order_id1, Side::Buy, 10, 50.0);
+  isin.add_order(order_id2, Side::Buy, 20, 70.0);
+  Proceeds p1 = Proceeds(0.0, 1900.0, 0, 0.0);
+  Proceeds p2 = isin.proceeds();
+  return p1 == p2;
+}
+
+bool test_isins()
+{
+  ISINs isins;
+  string isin_id("i1");
+  ISIN isin(isin_id);
+  string order_id1("o1");
+  string order_id2("o2");
+  isins.add_order(isin_id, order_id1, Side::Buy, 10, 50.0);
+  Proceeds p1 = Proceeds(0.0, 1900.0, 0, 0.0);
+  Proceeds p2 = isins.proceeds_with_order(isin_id, order_id2, Side::Buy, 20, 70.0);
+  return p1 == p2;
+}
+
+bool test_isins_first_proceeds()
+{
+  ISINs isins;
+  string isin_id("i1");
+  ISIN isin(isin_id);
+  string order_id1("o1");
+  Proceeds p1 = Proceeds(0.0, 1400.0, 0, 0.0);
+  Proceeds p2 = isins.proceeds_with_order(isin_id, order_id1, Side::Buy, 20, 70.0);
+  return p1 == p2;
+}
+
+bool test_account()
+{
+  string account_id("a1");
+  Account account(account_id);
+  string isin_id("i1");
+  string order_id1("o1");
+  string order_id2("o2");
+  account.add_order(isin_id, order_id1, Side::Buy, 10, 50.0);
+  account.add_order(isin_id, order_id2, Side::Buy, 20, 70.0);
+  Proceeds p1 = Proceeds(0.0, 1900.0, 0, 0.0);
+  Proceeds p2 = account.proceeds_for_isin(isin_id);
+  return p1 == p2;
+}
+
+bool test_accounts()
+{
+  Accounts accounts;
+  string account_id("a1");
+  string isin_id("i1");
+  string order_id1("o1");
+  string order_id2("o2");
+  accounts.add_order(account_id, isin_id, order_id1, Side::Buy, 10, 50.0);
+  Proceeds p1 = Proceeds(0.0, 1900.0, 0, 0.0);
+  Proceeds p2 = accounts.proceeds_with_order(account_id, isin_id, order_id2, Side::Buy, 20, 70.0);
+  return p1 == p2;
+}
+
+bool test_accounts_first_proceeds()
+{
+  Accounts accounts;
+  string account_id("a1");
+  string isin_id("i1");
+  string order_id1("o1");
+  Proceeds p1 = Proceeds(0.0, 1400.0, 0, 0.0);
+  Proceeds p2 = accounts.proceeds_with_order(account_id, isin_id, order_id1, Side::Buy, 20, 70.0);
+  return p1 == p2;
+}
+
+bool test_client()
+{
+  string client_id("c1");
+  Client client(client_id);
+  string account_id("a1");
+  string isin_id("i1");
+  string order_id1("o1");
+  string order_id2("o2");
+  client.add_order(account_id, isin_id, order_id1, Side::Buy, 10, 50.0);
+  client.add_order(account_id, isin_id, order_id2, Side::Buy, 20, 70.0);
+  Proceeds p1 = Proceeds(0.0, 1900.0, 0, 0.0);
+  Proceeds p2 = client.proceeds_for_isin(account_id, isin_id);
+  return p1 == p2;
+}
+
+bool test_clients()
+{
+  Clients clients;
+  string client_id("c1");
+  string account_id("a1");
+  string isin_id("i1");
+  string order_id1("o1");
+  string order_id2("o2");
+  clients.add_order(client_id, account_id, isin_id, order_id1, Side::Buy, 10, 50.0);
+  Proceeds p1 = Proceeds(0.0, 1900.0, 0, 0.0);
+  Proceeds p2 = clients.proceeds_with_order(client_id, account_id, isin_id, order_id2, Side::Buy, 20, 70.0);
+  return p1 == p2;
+}
+
+bool test_clients_first_proceeds()
+{
+  Clients clients;
+  string client_id("c1");
+  string account_id("a1");
+  string isin_id("i1");
+  string order_id1("o1");
+  Proceeds p1 = Proceeds(0.0, 1400.0, 0, 0.0);
+  Proceeds p2 = clients.proceeds_with_order(client_id, account_id, isin_id, order_id1, Side::Buy, 20, 70.0);
+  return p1 == p2;
+}
+
+#define TEST(TEST_NAME) if (TEST_NAME()) { printf("passed: " #TEST_NAME "\n"); } else { printf("FAILED: " #TEST_NAME "\n"); }
+
+int main(int argc, char** argv)
+{
+  TEST(test_proceeds);
+  TEST(test_proceeds_add);
+  TEST(test_order);
+  TEST(test_orders);
+  TEST(test_isin);
+  TEST(test_isins);
+  TEST(test_isins_first_proceeds);
+  TEST(test_account);
+  TEST(test_accounts);
+  TEST(test_accounts_first_proceeds)
+  TEST(test_client);
+  TEST(test_clients);
+  TEST(test_clients_first_proceeds);
+  return 0;
+}
---
+++ apps/arizona-source-app/hpp/ArizonaState.hpp
@@ -0,0 +1,131 @@
+#ifndef __ARIZONA_STATE_HPP__
+#define __ARIZONA_STATE_HPP__
+
+#include <string>
+#include <map>
+
+using std::string;
+using std::map;
+
+enum Side
+{
+  Buy = 0,
+  Sell = 1
+};
+
+class Proceeds
+{
+private:
+  double _proceeds_short;
+  double _proceeds_long;
+  double _open_short;
+  double _open_long;
+public:
+  Proceeds(double proceeds_short_, double proceeds_long_, double open_short_, double open_long_);
+  Proceeds& add(Proceeds& that);
+  bool operator==(Proceeds& that);
+};
+
+class Order
+{
+private:
+  string _order_id;
+  Side _side;
+  uint32_t _quantity;
+  double _price;
+public:
+  Order(string& order_id_, Side side_, uint32_t quantity_, double price_);
+  bool operator==(Order& that);
+  Proceeds proceeds();
+};
+
+class Orders
+{
+private:
+  map<string, Order *> _orders;
+public:
+  Orders();
+  ~Orders();
+  void add_order(string& order_id, Side side_, uint32_t quantity, double price_);
+  Proceeds proceeds();
+  size_t count() { return _orders.size(); }
+};
+
+class ISIN
+{
+private:
+  string _isin_id;
+  Orders _orders;
+public:
+  ISIN(string& isin_id_);
+  void add_order(string& _order_id_, Side side_, double quantity_, double price_);
+  Proceeds proceeds();
+};
+
+class ISINs
+{
+private:
+  map<string, ISIN*> _isins;
+  ISIN* _add_isin(string& isin_id_);
+  ISIN* _isin_by_isin_id(string& isin_id_);
+public:
+  ISINs();
+  ~ISINs();
+  void add_order(string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+  Proceeds proceeds_for_isin(string& isin_id_);
+  Proceeds proceeds_with_order(string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+};
+
+class Account
+{
+private:
+  string _account_id;
+  ISINs _isins;
+public:
+  Account(string& account_id_);
+  void add_order(string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+  Proceeds proceeds_for_isin(string& isin_id_);
+  Proceeds proceeds_with_order(string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+};
+
+class Accounts
+{
+private:
+  map<string, Account *> _accounts;
+  Account *_account_by_account_id(string& account_id_);
+  Account *_add_account(string& account_id_);
+public:
+  Accounts();
+  ~Accounts();
+  void add_order(string& account_id_, string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+  Proceeds proceeds_for_isin(string& account_id_, string& isin_id_);
+  Proceeds proceeds_with_order(string& account_id_, string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+};
+
+class Client
+{
+private:
+  Accounts _accounts;
+  string _client_id;
+public:
+  Client(string& client_id_);
+  void add_order(string& account_id_, string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+  Proceeds proceeds_for_isin(string& account_id_, string& isin_id_);
+  Proceeds proceeds_with_order(string& account_id_, string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+};
+
+class Clients
+{
+private:
+  map<string, Client*> _clients;
+  Client *_client_by_client_id(string& client_id_);
+  Client *_add_client(string& client_id_);
+public:
+  Clients();
+  ~Clients();
+  void add_order(string& client_id_, string& account_id_, string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+  Proceeds proceeds_for_isin(string& client_id, string& account_id_, string& isin_id_);
+  Proceeds proceeds_with_order(string& client_id_, string& account_id_, string& isin_id_, string& order_id_, Side side_, uint32_t quantity_, double price_);
+};
+
+#endif // __ARIZONA_STATE_HPP__
