a1ecb0c6ad11b7d3d76460a9e1eb6038f7c04771
Andrew Turley <aturley@acm.org>
2017-02-17 17:21:58 -0500

    Re-add StateChange::id() method
    This method was accidentally deleted.

--- lib/wallaroo/cpp_api/cpp/cppapi/hpp/StateChange.hpp
+++ lib/wallaroo/cpp_api/cpp/cppapi/hpp/StateChange.hpp
@@ -20,10 +20,7 @@ public:
   virtual size_t get_log_entry_size_header_size() = 0;
   virtual size_t read_log_entry_size_header(char *bytes_) = 0;
   virtual bool read_log_entry(char *bytes_) = 0;
-
-public:
   virtual uint64_t id() = 0;
-  virtual string str() = 0;
 };
 }
 
