fbe608c8e7561e0045be016c4187b359cca779f0
JONBRWN <jonathanbrown.a@gmail.com>
2017-02-28 17:07:17 -0500

    Update MonHub tests
    - removed unused code/tests from metrics channel
    - update AppConfigStore test

--- monitoring_hub/apps/metrics_reporter_ui/web/channels/metrics_channel.ex
+++ monitoring_hub/apps/metrics_reporter_ui/web/channels/metrics_channel.ex
@@ -28,57 +28,6 @@ defmodule MetricsReporterUI.MetricsChannel do
     {:noreply, socket}
   end
 
-  def handle_in("step-metrics", metrics_collection, socket) do
-    "metrics:" <> app_name = socket.topic
-    Enum.each(metrics_collection, fn (%{"pipeline_key" => pipeline_key,
-      "t0" => _start_timestamp, "t1" => end_timestamp,
-      "category" => "step" = category,
-      "topics" => %{"latency_bins" => latency_bins,
-      "throughput_out" => throughput_data}}) ->
-      int_end_timestamp = float_timestamp_to_int(end_timestamp)
-      latency_bins_msg = create_latency_bins_msg(pipeline_key, int_end_timestamp, latency_bins)
-      store_latency_bins_msg(app_name, category, pipeline_key, latency_bins_msg)
-      {:ok, _pid} = find_or_start_latency_bins_worker(app_name, category, pipeline_key)
-      store_throughput_msgs(app_name, category, pipeline_key, throughput_data)
-      {:ok, _pid} = find_or_start_throughput_workers(app_name, category, pipeline_key)
-    end)
-    {:reply, :ok, socket}
-  end
-
-  def handle_in("ingress-egress-metrics", metrics_collection, socket) do
-    "metrics:" <> app_name = socket.topic
-    Enum.each(metrics_collection, fn (%{"pipeline_key" => pipeline_key,
-      "t0" => _start_timestamp, "t1" => end_timestamp,
-      "category" => "ingress-egress" = category,
-      "topics" => %{"latency_bins" => latency_bins,
-      "throughput_out" => throughput_data}}) ->
-      int_end_timestamp = float_timestamp_to_int(end_timestamp)
-      latency_bins_msg = create_latency_bins_msg(pipeline_key, int_end_timestamp, latency_bins)
-      store_latency_bins_msg(app_name, category, pipeline_key, latency_bins_msg)
-      {:ok, _pid} = find_or_start_latency_bins_worker(app_name, category, pipeline_key)
-      store_throughput_msgs(app_name, category, pipeline_key, throughput_data)
-      {:ok, _pid} = find_or_start_throughput_workers(app_name, category, pipeline_key)
-    end)
-    {:reply, :ok, socket}
-  end
-
-  def handle_in("source-sink-metrics", metrics_collection, socket) do
-    "metrics:" <> app_name = socket.topic
-    Enum.each(metrics_collection, fn (%{"pipeline_key" => pipeline_key,
-      "t0" => _start_timestamp, "t1" => end_timestamp,
-      "category" => "source-sink" = category,
-      "topics" => %{"latency_bins" => latency_bins,
-      "throughput_out" => throughput_data}}) ->
-      int_end_timestamp = float_timestamp_to_int(end_timestamp)
-      latency_bins_msg = create_latency_bins_msg(pipeline_key, int_end_timestamp, latency_bins)
-      store_latency_bins_msg(app_name, category, pipeline_key, latency_bins_msg)
-      {:ok, _pid} = find_or_start_latency_bins_worker(app_name, category, pipeline_key)
-      store_throughput_msgs(app_name, category, pipeline_key, throughput_data)
-      {:ok, _pid} = find_or_start_throughput_workers(app_name, category, pipeline_key)
-    end)
-    {:reply, :ok, socket}
-  end
-
   defp float_timestamp_to_int(timestamp) do
     round(timestamp)
   end
