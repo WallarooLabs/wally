6678ee887f408c882721af2a9a4d94e954577133
Nisan Haramati <hanisan@gmail.com>
2017-05-03 13:09:51 -0700

    pep8 wallaroo.py

--- machida/wallaroo.py
+++ machida/wallaroo.py
@@ -2,7 +2,7 @@ class ApplicationBuilder(object):
     def __init__(self, name):
         self._actions = [("name", name)]
 
-    def new_pipeline(self, name, decoder, coalescing = True):
+    def new_pipeline(self, name, decoder, coalescing=True):
         self._actions.append(("new_pipeline", name, decoder, coalescing))
         return self
 
@@ -11,15 +11,21 @@ class ApplicationBuilder(object):
         return self
 
     def to_stateful(self, computation, state_builder, state_name):
-        self._actions.append(("to_stateful", computation, state_builder, state_name))
+        self._actions.append(("to_stateful", computation, state_builder,
+                              state_name))
         return self
 
-    def to_state_partition_u64(self, computation, state_builder, state_name, partition_function, partition_keys):
-        self._actions.append(("to_state_partition_u64", computation, state_builder, state_name, partition_function, partition_keys))
+    def to_state_partition_u64(self, computation, state_builder, state_name,
+                               partition_function, partition_keys):
+        self._actions.append(("to_state_partition_u64", computation,
+                              state_builder, state_name, partition_function,
+                              partition_keys))
         return self
 
-    def to_state_partition(self, computation, state_builder, state_name, partition_function, partition_keys):
-        self._actions.append(("to_state_partition", computation, state_builder, state_name, partition_function, partition_keys))
+    def to_state_partition(self, computation, state_builder, state_name,
+                           partition_function, partition_keys):
+        self._actions.append(("to_state_partition", computation, state_builder,
+                              state_name, partition_function, partition_keys))
         return self
 
     def to_sink(self, encoder):
