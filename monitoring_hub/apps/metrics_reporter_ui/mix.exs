2dbe39ef7c40317d72afcdaf716f3d18d98ce68f
JONBRWN <jonathanbrown.a@gmail.com>
2017-05-25 15:52:08 -0400

    Fix failing linux build due to compilation error in erlware_commons
    Updates Exrm dependency to 1.0.8 to fix compilation error in erlware_commons
    
    Pin Erlang to 18.3
    
    Done to prevent the installation of newer Erlang versions which may include
    breaking changes.

--- monitoring_hub/apps/metrics_reporter_ui/mix.exs
+++ monitoring_hub/apps/metrics_reporter_ui/mix.exs
@@ -38,7 +38,7 @@ defmodule MetricsReporterUI.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
-     {:exrm, "~> 1.0.0"},
+     {:exrm, "~> 1.0.8"},
      {:phoenix_tcp, git: "https://github.com/Sendence/phoenix_tcp.git", branch: "add-hub-protocol"},
      {:monitoring_hub_utils, in_umbrella: true},
      {:metrics_reporter, in_umbrella: true}]
