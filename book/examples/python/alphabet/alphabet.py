WallarooLabs/wally

commit 029cafc4dc34f18781aead6dcdb2f2098ae562e2

Handle larger vote counts in Python alphabet examples
The examples hit an error fairly quickly when running where the number
of votes for a letter is larger than the `L` or `I` max value that can
be encoded.

book/examples/python/alphabet/alphabet.py
@@ -73,4 +73,4 @@ class AddVotes(object):
 class Encoder(object):
     def encode(self, data):
         # data is a Votes
-        return struct.pack(">LsL", 5, data.letter, data.votes)
+        return struct.pack(">LsQ", 5, data.letter, data.votes)

book/examples/python/alphabet_partitioned/alphabet_partitioned.py
@@ -11,6 +11,10 @@ def application_setup(args):
     ab.new_pipeline("alphabet", Decoder())
     ab.to_state_partition(AddVotes(), LetterStateBuilder(), "letter state",
                           LetterPartitionFunction(), letter_partitions)
+    ab.to_state_partition(AddVotes(), LetterStateBuilder(), "letter state2",
+                          LetterPartitionFunction(), letter_partitions)
+    ab.to_state_partition(AddVotes(), LetterStateBuilder(), "letter state3",
+                          LetterPartitionFunction(), letter_partitions)
     ab.to_sink(Encoder())
     return ab.build()
 
@@ -78,4 +82,6 @@ class Encoder(object):
         # data is a Votes
         letter = data.letter
         votes = data.votes
-        return struct.pack(">I1sI", 5, data.letter, data.votes)
+        print "letter is " + str(letter)
+        print "votes is " + str(votes)
+        return struct.pack(">LsQ", 5, data.letter, data.votes)

commit f52739abff023df294afb1d0430b3c44792e0537

Fix outgoing message length issue in python alphabet examples
Commit 029cafc4dc34f18781aead6dcdb2f2098ae562e2 introduced a change to
the message format of the python alphabet example programs that
changed the length of the outgoing message, but the length in the
header was not updated to reflect this. This commit fixes that.

Commit 029cafc4dc34f18781aead6dcdb2f2098ae562e2 also accidentally
added two stateful computations to the pipeline. These have been
removed.

book/examples/python/alphabet/alphabet.py
@@ -73,4 +73,4 @@ class AddVotes(object):
 class Encoder(object):
     def encode(self, data):
         # data is a Votes
-        return struct.pack(">LsQ", 5, data.letter, data.votes)
+        return struct.pack(">LsQ", 9, data.letter, data.votes)

book/examples/python/alphabet_partitioned/alphabet_partitioned.py
@@ -11,10 +11,6 @@ def application_setup(args):
     ab.new_pipeline("alphabet", Decoder())
     ab.to_state_partition(AddVotes(), LetterStateBuilder(), "letter state",
                           LetterPartitionFunction(), letter_partitions)
-    ab.to_state_partition(AddVotes(), LetterStateBuilder(), "letter state2",
-                          LetterPartitionFunction(), letter_partitions)
-    ab.to_state_partition(AddVotes(), LetterStateBuilder(), "letter state3",
-                          LetterPartitionFunction(), letter_partitions)
     ab.to_sink(Encoder())
     return ab.build()
 
@@ -84,4 +80,4 @@ class Encoder(object):
         votes = data.votes
         print "letter is " + str(letter)
         print "votes is " + str(votes)
-        return struct.pack(">LsQ", 5, data.letter, data.votes)
+        return struct.pack(">LsQ", 9, data.letter, data.votes)