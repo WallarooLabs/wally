96e0d9205545c9b609cec8e4bdfa80f164d0d387
JONBRWN <jonathanbrown.a@gmail.com>
2017-06-28 22:33:43 -0400

    Pin Erlang/Elixir for OSX in Travix CI
    - pin Erlang to v18
    - pin Elixir to v1.2.6
    - comment back in failing UI tests due to Erlang v20 deprecation
      of crypto.rand_bytes

--- monitoring_hub/apps/metrics_reporter_ui/test/controllers/page_controller_test.exs
+++ monitoring_hub/apps/metrics_reporter_ui/test/controllers/page_controller_test.exs
@@ -1,10 +1,8 @@
 defmodule MetricsReporterUI.PageControllerTest do
   use MetricsReporterUI.ConnCase
-  # FIX ME: Comment back in once Erlang/Elixir are pinned on OSX
-  #	currently fails due to Erlang 20 being installed by Travis CI
-  # and :crypto.rand_bytes being deprecated
-  # test "GET /", %{conn: conn} do
-  #   conn = get conn, "/"
-  #   assert html_response(conn, 200) =~ "Metrics Reporter"
-  # end
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Metrics Reporter"
+  end
 end
