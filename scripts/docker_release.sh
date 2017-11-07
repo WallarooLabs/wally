bf875fdf90ffbdc87a3352def9519eba92cc049d
JONBRWN <jonathanbrown.a@gmail.com>
2017-11-07 15:48:02 -0500

    Adds automation for building/releasing Docker image via Travis
    Updates travis.yml to build and release the Wallaroo docker image
    on push and PR to the release-* and release branches. Adds a
    docker_release.sh script which checks whether an image with the same tag
    exists before building and pushing to the Bintray repo, this is to avoid
    overwriting existing docker images and to save time on CI builds.
    
    Adds commands needed in order to build the Wallaroo docker image
    in Travis.

---
+++ scripts/docker_release.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+set -x
+set -o errexit
+set -o nounset
+
+if [[ "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_BRANCH" != "master" ]]
+then
+	echo "Building Wallaroo docker image and pushing to Bintray..."
+	## Sets Wallaroo Docker repo host to Bintray repo.
+	wallaroo_docker_repo_host=wallaroo-labs-docker-wallaroolabs.bintray.io
+	## Sets version variable to first version found in CHANGELOG
+	version=`grep -Po '(?<=##\s\[).*(?=\])' CHANGELOG.md | head -1`
+	## If version is unreleased, use commit as tag. Otherwise use version.
+	if [[ $version == "unreleased" ]]
+	then
+		## Sets repo to dev for Wallaroo Docker image
+		wallaroo_docker_image_repo=dev
+		docker_image_tag=$(git describe --tags --always)
+	else
+		## Sets repo to first-install for Wallaroo Docker image
+		wallaroo_docker_image_repo=first-install
+		docker_image_tag=$version
+	fi
+	wallaroo_docker_image=wallaroo:$docker_image_tag
+	wallaroo_docker_image_path=$wallaroo_docker_repo_host/$wallaroo_docker_image_repo/$wallaroo_docker_image
+	## Conditional check for whether current image tag exists in repo, does not
+	## re-upload image if so. Otherwise builds docker image and uploads to Bintray.
+	returned_tag_name=$(curl -s "https://$wallaroo_docker_repo_host/v2/$wallaroo_docker_image_repo/wallaroo/tags/list" | jq ".tags" | grep -Po "(?<=\")$docker_image_tag(?=\")" || echo "0")
+	if [[ "$returned_tag_name" == "$docker_image_tag" ]]
+	then
+		echo "Docker image: $wallaroo_docker_image_path already exists"
+		exit 0
+	else
+		docker login -u wallaroolabs -p $DOCKER_PASSWORD $wallaroo_docker_repo_host
+		make release-monhub-all
+		docker build -t $wallaroo_docker_image_path .
+		docker push $wallaroo_docker_image_path
+		echo "Built and pushed image $wallaroo_docker_image_path successfully."
+	fi
+else
+	echo "Not building Wallaroo docker image."
+	exit 0
+fi
