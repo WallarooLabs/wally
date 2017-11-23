19a2913f736c605097775c519b637713532c054c
Sean T Allen <sean@monkeysnatchbanana.com>
2017-11-23 00:00:21 -0500

    WIP

--- travis/install.sh
+++ travis/install.sh
@@ -49,6 +49,7 @@ install_ponyc() {
       sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "8756 C4F7 65C9 AC3C B6B8  5D62 379C E192 D401 AB61"
       echo "deb https://dl.bintray.com/pony-language/ponyc-debian pony-language main" | sudo tee -a /etc/apt/sources.list
       sudo apt-get update
+      sudo apt-cache policy ponyc
       sudo apt-get -V install ponyc=$PONYC_VERSION
     ;;
 
