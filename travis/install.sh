44826b34f834395d72073b0bc245b94cef9c9e96
Sean T Allen <sean@monkeysnatchbanana.com>
2017-11-23 10:05:02 -0500

    Remove left over Travis debugging command
    [skip ci]

--- travis/install.sh
+++ travis/install.sh
@@ -49,7 +49,6 @@ install_ponyc() {
       sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "8756 C4F7 65C9 AC3C B6B8  5D62 379C E192 D401 AB61"
       echo "deb https://dl.bintray.com/pony-language/ponyc-debian pony-language main" | sudo tee -a /etc/apt/sources.list
       sudo apt-get update
-      sudo apt-cache policy ponyc
       # this temporarily doesn't work.
       #sudo apt-get -V install ponyc=$PONYC_VERSION
       # temporarily install latest until ponyc as sane version numbers
