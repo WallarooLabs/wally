8b63029ece58ed16f3cb72091f83cd4169e7efac
Sean T Allen <sean@monkeysnatchbanana.com>
2017-11-23 10:04:05 -0500

    Allow for installing arbitrary ponyc versions from source with TravisCI
    Set INSTALL_PONYC_FROM to "source"
    Set PONYC_VERSION to the tag to use when running `git checkout`

--- travis/install.sh
+++ travis/install.sh
@@ -3,53 +3,97 @@
 set -o errexit
 set -o nounset
 
-download_llvm() {
-  echo "Downloading and installing the LLVM specified by envvars..."
+install_llvm() {
+  echo "** Downloading and installing LLVM ${LLVM_VERSION}"
 
+  pushd /tmp
   wget "http://llvm.org/releases/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-debian8.tar.xz"
   tar -xvf clang+llvm*
   pushd clang+llvm* && sudo mkdir /tmp/llvm && sudo cp -r ./* /tmp/llvm/
   sudo ln -s "/tmp/llvm/bin/llvm-config" "/usr/local/bin/${LLVM_CONFIG}"
   popd
+  popd
+
+  echo "** LLVM ${LLVM_VERSION} installed"
 }
 
-download_pcre() {
-  echo "Downloading and building PCRE2..."
+install_pcre() {
+  echo "** Downloading and building PCRE2..."
 
+  pushd /tmp
   wget "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.21.tar.bz2"
   tar -xjvf pcre2-10.21.tar.bz2
   pushd pcre2-10.21 && ./configure --prefix=/usr && make && sudo make install
   popd
+  popd
+
+  echo "** PRCE2 installed"
 }
 
 install_cpuset() {
   # if cpuset isn't installed, we get warning messages that dirty up
   # the travis output logs
+  echo "** Installing cpuset"
+
   sudo apt-get -fy install cpuset
+
+  echo "** cpuset installed"
 }
 
 install_ponyc() {
-  echo "Installing ponyc"
-  if [[ "$INSTALL_PONYC_FROM" == "last-release" ]]
-  then
-    echo "Installing latest ponyc release from bintray"
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "8756 C4F7 65C9 AC3C B6B8  5D62 379C E192 D401 AB61"
-    echo "deb https://dl.bintray.com/pony-language/ponyc-debian pony-language main" | sudo tee -a /etc/apt/sources.list
-    sudo apt-get update
-    sudo apt-get -V install ponyc
-  fi
+  echo "** Installing ponyc from ${INSTALL_PONYC_FROM}"
+
+  case "$INSTALL_PONYC_FROM" in
+    "bintray")
+      echo "Installing latest ponyc release from bintray"
+      sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "8756 C4F7 65C9 AC3C B6B8  5D62 379C E192 D401 AB61"
+      echo "deb https://dl.bintray.com/pony-language/ponyc-debian pony-language main" | sudo tee -a /etc/apt/sources.list
+      sudo apt-get update
+      sudo apt-cache policy ponyc
+      # this temporarily doesn't work.
+      #sudo apt-get -V install ponyc=$PONYC_VERSION
+      # temporarily install latest until ponyc as sane version numbers
+      sudo apt-get -V install ponyc
+    ;;
+
+    "source")
+      echo "Installing ponyc dependencies"
+      install_llvm
+      install_pcre
+      echo "Installing ponyc from source"
+      pushd /tmp
+      git clone https://github.com/ponylang/ponyc.git
+      pushd ponyc
+      git checkout $PONYC_VERSION
+      make CC=$CC1 CXX=$CXX1
+      sudo make install
+      popd
+      popd
+    ;;
+
+    *)
+      echo "ERROR: unrecognized source to install ponyc from ${INSTALL_PONYC_FROM}"
+      exit 1
+    ;;
+  esac
+
+  echo "** ponyc installed"
 }
 
 install_pony_stable() {
-  echo "Installing latest pony-stable release from bintray"
+  echo "** Installing pony-stable"
+
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys "D401AB61 DBE1D0A2"
   echo "deb https://dl.bintray.com/pony-language/pony-stable-debian /" | sudo tee -a /etc/apt/sources.list
   sudo apt-get update
   sudo apt-get -V install pony-stable
+
+  echo "** pony-stable installed"
 }
 
 install_kafka_compression_libraries() {
-  echo "Installing compression libraries needed for Kafka support"
+  echo "** Installing compression libraries needed for Kafka support"
+
   sudo apt-get install libsnappy-dev
   pushd /tmp
   wget -O liblz4-1.7.5.tar.gz https://github.com/lz4/lz4/archive/v1.7.5.tar.gz
@@ -58,17 +102,19 @@ install_kafka_compression_libraries() {
   sudo make install
   popd
   popd
+
+  echo "** Compression libraries needed for Kafka support installed"
 }
 
 install_monitoring_hub_dependencies() {
-  echo "Installing monitoring hub dependencies"
+  echo "** Installing monitoring hub dependencies"
+
   echo "deb https://packages.erlang-solutions.com/ubuntu trusty contrib" | sudo tee -a /etc/apt/sources.list
   pushd /tmp
   wget https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
   sudo apt-key add erlang_solutions.asc
   popd
   sudo apt-get update
-  #sudo apt-get -fy install erlang=1:18.3
   sudo apt-get -fy install erlang-base=1:18.0 erlang-dev=1:18.0 \
     erlang-parsetools=1:18.0 erlang-eunit=1:18.0 erlang-crypto=1:18.0 \
     erlang-syntax-tools=1:18.0 erlang-asn1=1:18.0 erlang-public-key=1:18.0 \
@@ -77,15 +123,22 @@ install_monitoring_hub_dependencies() {
   echo "Install keix and setup Elixir version"
   kiex install 1.2.6
   kiex default 1.2.6
+
+  echo "** Monitoring hub dependencies installed"
 }
 
 install_python_dependencies() {
-  echo "Installing python dependencies"
+  echo "** Installing python dependencies"
+
   echo "Installing pytest"
   sudo python2 -m pip install pytest==3.2.2
   echo "Install enum"
   sudo python2 -m pip install --upgrade pip enum34
+
+  echo "** Python dependencies installed"
 }
+
+echo "----- Installing dependencies"
 
 install_cpuset
 install_ponyc
@@ -93,3 +146,5 @@ install_pony_stable
 install_kafka_compression_libraries
 install_monitoring_hub_dependencies
 install_python_dependencies
+
+echo "----- Dependencies installed"
