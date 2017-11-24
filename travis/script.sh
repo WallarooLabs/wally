0fb55ff57cbe9b30e1be3720806513b7b71242d0
Sean T Allen <sean@monkeysnatchbanana.com>
2017-11-24 11:41:20 -0500

    Move travis CI script commands to a bash script
    Easier to manage. Better error reporting. Can exit out of commands
    earlier.

---
+++ travis/script.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+set -o errexit
+set -o nounset
+
+make test debug=true
+# Make sure to run `make clean` between separate test runs to clear any conflicting dependencies
+make clean
+# Run the correctness tests that require a resilience build
+make integration-tests-testing-correctness-tests-all resilience=on debug=true
+# Release Wallaroo docker image (if needed)
+bash travis/docker_release.sh
