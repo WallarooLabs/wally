3c7b431b288f7a64fa702bb1ffac50b7ee32804f
JONBRWN <jonathanbrown.a@gmail.com>
2017-12-08 10:47:04 -0500

    Update docker_release script to be more robust
    This update will cause the docker_release script to build and
    release the Wallaroo Docker image only for the release and
    release-* branches. Adds a VERSION file to use to get
    the version of Wallaroo instead of relying on the CHANGELOG.

--- travis/docker_release.sh
+++ travis/docker_release.sh
@@ -3,23 +3,22 @@ set -x
 set -o errexit
 set -o nounset
 
-if [[ "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_BRANCH" != "master" ]]
+if [[ "$TRAVIS_BRANCH" == "release" || "$TRAVIS_BRANCH" == *"release-"* ]]
 then
 	echo "Building Wallaroo docker image and pushing to Bintray..."
 	## Sets Wallaroo Docker repo host to Bintray repo.
 	wallaroo_docker_repo_host=wallaroo-labs-docker-wallaroolabs.bintray.io
-	## Sets version variable to first version found in CHANGELOG
-	version=`grep -Po '(?<=##\s\[).*(?=\])' CHANGELOG.md | head -1`
-	## If version is unreleased, use commit as tag. Otherwise use version.
-	if [[ $version == "unreleased" ]]
+	if [[ "$TRAVIS_BRANCH" == "release" && "$TRAVIS_PULL_REQUEST" == "false" ]]
+	then
+		# Set version to output of VERSION file
+		version=$(< VERSION)
+		wallaroo_docker_image_repo=release
+		docker_image_tag=$version
+	elif [[ "$TRAVIS_BRANCH" == *"release-"* && "$TRAVIS_PULL_REQUEST" == "false" ]]
 	then
 		## Sets repo to dev for Wallaroo Docker image
 		wallaroo_docker_image_repo=dev
 		docker_image_tag=$(git describe --tags --always)
-	else
-		## Sets repo to first-install for Wallaroo Docker image
-		wallaroo_docker_image_repo=first-install
-		docker_image_tag=$version
 	fi
 	wallaroo_docker_image=wallaroo:$docker_image_tag
 	wallaroo_docker_image_path=$wallaroo_docker_repo_host/$wallaroo_docker_image_repo/$wallaroo_docker_image
@@ -28,16 +27,13 @@ then
 	returned_tag_name=$(curl -s "https://$wallaroo_docker_repo_host/v2/$wallaroo_docker_image_repo/wallaroo/tags/list" | jq ".tags" | grep -Po "(?<=\")$docker_image_tag(?=\")" || echo "0")
 	if [[ "$returned_tag_name" == "$docker_image_tag" ]]
 	then
-		echo "Docker image: $wallaroo_docker_image_path already exists"
+		echo "Docker image: $wallaroo_docker_image_path already exists."
 		exit 0
 	else
 		docker login -u wallaroolabs -p $DOCKER_PASSWORD $wallaroo_docker_repo_host
-		make release-monhub-all
+		make release-monitoring_hub-apps-metrics_reporter_ui
 		docker build -t $wallaroo_docker_image_path .
 		docker push $wallaroo_docker_image_path
 		echo "Built and pushed image $wallaroo_docker_image_path successfully."
 	fi
-else
-	echo "Not building Wallaroo docker image."
-	exit 0
 fi
