eef69055842795a68e6fb642933556909959cbc2
JONBRWN <jonathanbrown.a@gmail.com>
2017-12-12 11:43:14 -0500

    Add links to Metrics UI
    Includes link for Wallaroo survey in footer and Version check link on the
    Applications page.

---
+++ monitoring_hub/apps/metrics_reporter_ui/web/static/js/buffy-ui/components/applications/VersionAlert.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Alert, Button } from "react-bootstrap"
+
+export default class VersionAlert extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			alertVisible: true,
+		};
+	}
+	handleAlertDismiss() {
+	  this.setState({ alertVisible: false });
+	}
+
+	render() {
+		if (this.state.alertVisible) {
+		     return (
+		        <Alert bsStyle="info">
+		          <p>You're on version <strong>0.3.0</strong>, click <a href="https://www.wallaroolabs.com/ui/latest?version=0.3.0" target="_blank">here</a> to verify you're up to date!</p>
+		        </Alert>
+		     );
+		}
+		return(<div></div>);
+	}
+
+}
