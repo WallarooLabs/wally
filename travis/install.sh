fe6fa10f5e23887d5ad1908e296eae0ba2787618
JONBRWN <jonathanbrown.a@gmail.com>
2017-12-14 17:30:37 -0500

    Print O_NOBLOCK flag prior to untarring

--- travis/install.sh
+++ travis/install.sh
@@ -8,6 +8,7 @@ install_llvm() {
 
   pushd /tmp
   wget "http://llvm.org/releases/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-debian8.tar.xz"
+  python -c 'import os,sys,fcntl; flags = fcntl.fcntl(sys.stdout, fcntl.F_GETFL); print(flags&os.O_NONBLOCK);'
   tar -xvf clang+llvm*
   pushd clang+llvm* && sudo mkdir /tmp/llvm && sudo cp -r ./* /tmp/llvm/
   sudo ln -s "/tmp/llvm/bin/llvm-config" "/usr/local/bin/${LLVM_CONFIG}"
