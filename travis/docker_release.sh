23847e8efff6bbe6d4b22ad8a8651daf4a14445f
Sean T Allen <sean@monkeysnatchbanana.com>
2018-01-04 20:46:41 -0500

    Don't run deploy task for pull requests

--- travis/docker_release.sh
+++ travis/docker_release.sh
@@ -1,39 +1,36 @@
-#/bin/bash
+#! /bin/bash
 set -x
 set -o errexit
 set -o nounset
 
-if [[ "$TRAVIS_BRANCH" == "release" || "$TRAVIS_BRANCH" == *"release-"* ]]
+echo "Building Wallaroo docker image and pushing to Bintray..."
+## Sets Wallaroo Docker repo host to Bintray repo.
+wallaroo_docker_repo_host=wallaroo-labs-docker-wallaroolabs.bintray.io
+if [[ "$TRAVIS_BRANCH" == "release" ]]
 then
-	echo "Building Wallaroo docker image and pushing to Bintray..."
-	## Sets Wallaroo Docker repo host to Bintray repo.
-	wallaroo_docker_repo_host=wallaroo-labs-docker-wallaroolabs.bintray.io
-	if [[ "$TRAVIS_BRANCH" == "release" && "$TRAVIS_PULL_REQUEST" == "false" ]]
-	then
-		# Set version to output of VERSION file
-		version=$(< VERSION)
-		wallaroo_docker_image_repo=release
-		docker_image_tag=$version
-	elif [[ "$TRAVIS_BRANCH" == *"release-"* && "$TRAVIS_PULL_REQUEST" == "false" ]]
-	then
-		## Sets repo to dev for Wallaroo Docker image
-		wallaroo_docker_image_repo=dev
-		docker_image_tag=$(git describe --tags --always)
-	fi
-	wallaroo_docker_image=wallaroo:$docker_image_tag
-	wallaroo_docker_image_path=$wallaroo_docker_repo_host/$wallaroo_docker_image_repo/$wallaroo_docker_image
-	## Conditional check for whether current image tag exists in repo, does not
-	## re-upload image if so. Otherwise builds docker image and uploads to Bintray.
-	returned_tag_name=$(curl -s "https://$wallaroo_docker_repo_host/v2/$wallaroo_docker_image_repo/wallaroo/tags/list" | jq ".tags" | grep -Po "(?<=\")$docker_image_tag(?=\")" || echo "0")
-	if [[ "$returned_tag_name" == "$docker_image_tag" ]]
-	then
-		echo "Docker image: $wallaroo_docker_image_path already exists."
-		exit 0
-	else
-		docker login -u wallaroolabs -p $DOCKER_PASSWORD $wallaroo_docker_repo_host
-		make release-monitoring_hub-apps-metrics_reporter_ui
-		docker build -t $wallaroo_docker_image_path .
-		docker push $wallaroo_docker_image_path
-		echo "Built and pushed image $wallaroo_docker_image_path successfully."
-	fi
+	# Set version to output of VERSION file
+	version=$(< VERSION)
+	wallaroo_docker_image_repo=release
+	docker_image_tag=$version
+elif [[ "$TRAVIS_BRANCH" == *"release-"* ]]
+then
+	## Sets repo to dev for Wallaroo Docker image
+	wallaroo_docker_image_repo=dev
+	docker_image_tag=$(git describe --tags --always)
+fi
+wallaroo_docker_image=wallaroo:$docker_image_tag
+wallaroo_docker_image_path=$wallaroo_docker_repo_host/$wallaroo_docker_image_repo/$wallaroo_docker_image
+## Conditional check for whether current image tag exists in repo, does not
+## re-upload image if so. Otherwise builds docker image and uploads to Bintray.
+returned_tag_name=$(curl -s "https://$wallaroo_docker_repo_host/v2/$wallaroo_docker_image_repo/wallaroo/tags/list" | jq ".tags" | grep -Po "(?<=\")$docker_image_tag(?=\")" || echo "0")
+if [[ "$returned_tag_name" == "$docker_image_tag" ]]
+then
+	echo "Docker image: $wallaroo_docker_image_path already exists."
+	exit 0
+else
+	docker login -u wallaroolabs -p $DOCKER_PASSWORD $wallaroo_docker_repo_host
+	make release-monitoring_hub-apps-metrics_reporter_ui
+	docker build -t $wallaroo_docker_image_path .
+	docker push $wallaroo_docker_image_path
+	echo "Built and pushed image $wallaroo_docker_image_path successfully."
 fi
