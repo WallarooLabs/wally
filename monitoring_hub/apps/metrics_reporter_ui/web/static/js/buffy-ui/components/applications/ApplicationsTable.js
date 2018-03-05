1b968a35720b2d691878b315e5cf203beb5c0ee4
JONBRWN <jonathan@sendence.com>
2018-03-05 13:22:47 -0500

    Add "No applications connected" message to Apps page in Metrics UI (#2080)
    From a users experience perspective, visiting the Apps page for the
    first time should notify the user that no apps are connected instead
    of being an empty page. This commit adds a "No applications connected"
    message and is removed once an application connects to the MonHub.
    
    closes #2048

--- monitoring_hub/apps/metrics_reporter_ui/web/static/js/buffy-ui/components/applications/ApplicationsTable.js
+++ monitoring_hub/apps/metrics_reporter_ui/web/static/js/buffy-ui/components/applications/ApplicationsTable.js
@@ -1,15 +1,22 @@
 import React from "react"
 import { Table, Alert } from "react-bootstrap"
 import ApplicationsTableRow from "./ApplicationsTableRow"
+import EmptyAppsRow from "./EmptyAppsRow"
 import VersionAlert from "./VersionAlert"
+import { List } from "immutable"
 
 export default class ApplicationsTable extends React.Component {
 	render() {
+		let rows;
 		const { appConfigs } = this.props;
-		const rows =  appConfigs.map(function(appConfig) {
-			const appName = appConfig.get("app_name");
-			return <ApplicationsTableRow appName={appName} key={appName} />;
-		});
+		if (appConfigs.isEmpty()) {
+			rows = List([<EmptyAppsRow />])
+		} else {
+			rows =  appConfigs.map(function(appConfig) {
+				const appName = appConfig.get("app_name");
+				return <ApplicationsTableRow appName={appName} key={appName} />;
+			});
+		}
 		return (
 			<div>
 			<VersionAlert />
