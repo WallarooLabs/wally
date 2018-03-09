4296e233463675db829da11bfd35a9a238c5ce04
Andrew Turley <aturley@acm.org>
2018-03-09 17:49:38 -0500

    Remove C++ API and examples
    The C++ API is not supported and has not been maintained. They should
    be removed from the repo so that they do not cause confusion.
    
    The API and examples directories have been moved to a private repo for
    safekeeping.

--- cpp_api/eclipse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# -gt 0 ] ; then
-    echo "     use this script to generate eclipse project tools"
-    echo "     to import the project, follow these instructions:"
-    echo "     https://cmake.org/Wiki/CMake_Generator_Specific_Information"
-    exit 1
-fi
-
-cmake -G"Eclipse CDT4 - Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug `pwd`
