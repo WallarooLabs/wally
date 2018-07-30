94f015380d78f9a1f01ff72c17863e56a857ff38
Simon Zelazny <zelazny@mailbox.org>
2018-07-30 11:46:43 -0700

    add reproducible example

---
+++ testing/correctness/apps/parallel_bug/parallel_test.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+import os
+import wallaroo
+import time
+
+N_PARTITIONS=4
+MAX=1000
+NoneClass=namedtuple("NoneClass", [])
+
+def application_setup(args):
+    # wallaroo pipeline
+    in_host, in_port = wallaroo.tcp_parse_input_addrs(args)[0]
+    out_host, out_port = wallaroo.tcp_parse_output_addrs(args)[0]
+    tcp_source = wallaroo.TCPSourceConfig(in_host, in_port, decoder)
+    tcp_sink = wallaroo.TCPSinkConfig(out_host, out_port, encoder)
+
+    ab = wallaroo.ApplicationBuilder("Parallel stateless test app")
+    ab.new_pipeline("Parallel test app", tcp_source)
+    # Comment out to_stateful to see round-robin
+    # Leave it in place to see no round-robin
+    ab.to_stateful(const, NoneClass, "whatever")
+    ab.to(const_list)
+    ab.to_parallel(report_executing_node),
+    ab.to_sink(tcp_sink)
+    return ab.build()
+
+@wallaroo.state_computation(name="return the same thing")
+def const(_a, _b):
+    return (1, True)
+
+@wallaroo.computation_multi(name="return a list")
+def const_list(_):
+    return [1,2,3,4]
+
+@wallaroo.computation(name="Report which node got which record")
+def report_executing_node(rows):
+    report = "%s NODE %s got chunk\n"%(time.clock(), os.getpid())
+    return report
+
+@wallaroo.decoder(header_length=4, length_fmt=">I")
+def decoder(_any):
+    return 1
+
+@wallaroo.encoder
+def encoder(report):
+    return report
