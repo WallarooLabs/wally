62d26b83e742777502ba49132910025c76b478e3
Scott Lystig Fritchie <slfritchie@snookles.com>
2018-09-15 14:08:30 -0500

    Add utils/resilience-demo scripts

--- utils/resilience-demo/START-DOS-SERVER.sh
+++ utils/resilience-demo/START-DOS-SERVER.sh
@@ -5,7 +5,7 @@
 if [ $USE_DOS_SERVER = y ]; then
     echo Start DOS server
     ssh -n $USER@$DOS_SERVER_EXT "rm -rf /tmp/dos-data ; mkdir /tmp/dos-data"
-    ssh -n $USER@$DOS_SERVER_EXT "cd wallaroo ; python ./utils/dos-dumb-object-service/dos-server.py /tmp/dos-data > /tmp/dos-data/errout.txt 2>&1" &
+    ssh -n $USER@$DOS_SERVER_EXT "cd wallaroo ; python ./utils/dos-dumb-object-service/dos-server.py /tmp/dos-data > /tmp/dos-data/errout.txt 2>&1" > /dev/null 2&>1 &
     W_DOS_SERVER_ARG="--resilience-dos-server $DOS_SERVER:9999"
 else
     echo Skip starting DOS server
