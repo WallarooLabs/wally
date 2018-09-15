5e7cbadb954c2103a501734c8492879c466b1973
Scott Lystig Fritchie <slfritchie@snookles.com>
2018-09-15 14:08:30 -0500

    Add utils/resilience-demo scripts

--- utils/resilience-demo/VERIFY3.sh
+++ utils/resilience-demo/VERIFY3.sh
@@ -18,17 +18,16 @@ export START_SENDER_CMD2='env PYTHONPATH="./testing/tools/integration" \
 
 . ./COMMON.sh
 
-echo To stop everything, run: env WALLAROO_BIN="./testing/correctness/apps/multi_partition_detector/multi_partition_detector" ./99-stop-everything.sh
-
-./20-start-2worker-cluster.sh
+./21-start-3worker-cluster.sh
 if [ $? -ne 0 ]; then
     echo STOP with non-zero status
 fi
 
 env START_SENDER_CMD="$START_SENDER_CMD1" START_SENDER_BG=n \
     ./30-start-sender.sh
-echo First sender has finished
-echo BONUS SLEEP 4; sleep 4
+echo First sender has finished; sleep 1
+echo BONUS SLEEP 5; sleep 5
+exit 99
 
 echo Kill worker2
 ./40-kill-worker.sh 2
@@ -49,17 +48,9 @@ echo Kill worker2
 echo Restart worker2 on server 2 with same TCP ports
 ssh -n $USER@$SERVER2_EXT "mkdir -p /tmp/run-dir/OLD ; mv -v /tmp/run-dir/m* /tmp/run-dir/OLD"
 ./62-restart-worker-no-port-change.sh 2 2
-if [ $? -ne 0 ]; then
-    exit 1
-fi
 
 env START_SENDER_CMD="$START_SENDER_CMD2" START_SENDER_BG=n \
   ./30-start-sender.sh
-S=3; echo Second sender has finished, sleep $S; sleep $S
+echo Second sender has finished; sleep 1
 
-echo Run validator to check for sequence validity.
-ssh -n $USER@$SERVER1_EXT "cd wallaroo; ./testing/correctness/apps/multi_partition_detector/validator/validator -e 2000  -i -k key_0,key_1,key_2,key_3,key_4,key_5,key_6,key_7,key_8,key_9,key_10"
-STATUS=$?
-
-echo Validation status was: $STATUS
-exit $STATUS
+echo To stop everything, run: env WALLAROO_BIN=$WALLAROO_BIN ./99-stop-everything.sh
