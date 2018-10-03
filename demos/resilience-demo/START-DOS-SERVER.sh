ae3112d1abb789271ca37416794d214117609468
Scott Lystig Fritchie <slfritchie@gmail.com>
2018-10-03 12:12:20 -0500

    Fix script paths in move from utils -> demos subdir (#2505)

--- demos/resilience-demo/START-DOS-SERVER.sh
+++ demos/resilience-demo/START-DOS-SERVER.sh
@@ -8,7 +8,7 @@ if [ "$USE_DOS_SERVER" = y ]; then
     else
         echo Start DOS server
         ssh -n $USER@$DOS_SERVER_EXT "rm -rf /tmp/dos-data ; mkdir /tmp/dos-data"
-        ssh -n $USER@$DOS_SERVER_EXT "cd wallaroo ; python ./utils/dos-dumb-object-service/dos-server.py /tmp/dos-data > /tmp/dos-data/errout.txt 2>&1" > /dev/null 2>&1 &
+        ssh -n $USER@$DOS_SERVER_EXT "cd wallaroo ; python ./testing/tools/dos-dumb-object-service/dos-server.py /tmp/dos-data > /tmp/dos-data/errout.txt 2>&1" > /dev/null 2>&1 &
     fi
     W_DOS_SERVER_ARG="--resilience-enable-io-journal --resilience-dos-server $DOS_SERVER:9999"
 else
