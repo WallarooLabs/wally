26cf1d088da6750ef860b38e9517988999718205
Nisan Haramati <hanisan@gmail.com>
2018-10-05 16:19:38 -0700

    Fix sequence_window_python test
    - struct.pack requires bytestring, not unicode, so encode the string
      first
    - check python's `isatty` status to determine whether we need to replace
      its stdout/stderr objects with ones that _do_ flush on each write

--- machida/lib/wallaroo/__init__.py
+++ machida/lib/wallaroo/__init__.py
@@ -21,6 +21,28 @@ import sys
 
 import wallaroo.experimental
 
+
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def writelines(self, datas):
+       self.stream.writelines(datas)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+
+# If python is running with PIPE stdout/stderr, replace them with
+# ones that always flush
+if not sys.stdout.isatty():
+    sys.stdout = Unbuffered(sys.stdout)
+if not sys.stderr.isatty():
+    sys.stderr = Unbuffered(sys.stderr)
+
+
 def serialize(o):
     return pickle.dumps(o)
 
