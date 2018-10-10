0cea7ac8384e490ade6bc4f7f0d596f14ce8bd21
Scott Lystig Fritchie <slfritchie@snookles.com>
2018-10-10 17:03:43 -0500

    Workaround for loopback/localhost mode: 40-kill-worker.sh aborts if machine # is not 2

--- demos/resilience-demo/40-kill-worker.sh
+++ demos/resilience-demo/40-kill-worker.sh
@@ -9,4 +9,11 @@ else
     eval 'TARGET_EXT=$SERVER'$1'_EXT'
 fi
 
+if [ $TARGET_EXT = 127.0.0.1 -o $TARGET_EXT = localhost ]; then
+    if [ $1 -ne 2 ]; then
+        echo "Sorry, this script doesn't work correctly in localhost/loopback style when target machine # is not equal to 2."
+        exit 1
+    fi
+fi
+
 ssh -n $USER@$TARGET_EXT 'PID=`ps axww | grep worker'$1' | egrep -v "cd wallaroo|grep" | awk '\''{print $1}'\''`; echo Pid is $PID; kill -9 $PID'
