42da6ddbf3048a2d4cab9f2db6ef4cf315c950fe
Nisan Haramati <hanisan@gmail.com>
2018-10-12 15:59:04 -0700

    Add python resilience and autoscale tests
    - Split autoscale tests off from resilience, and put them in
      `testing/correctness/tests/autoscale`
    - Use new detector for both resilience and autoscale tests
    - Run tests for both Python2 and Pony
    - Leave stubs in place for python3
    
    closes #2455

--- testing/correctness/tests/resilience/resilience_tests.py
+++ testing/correctness/tests/resilience/resilience_tests.py
@@ -4,122 +4,68 @@ from resilience import (Crash,
                         Shrink,
                         Wait,
                         _test_resilience)
+from test_creator import Creator
+
+import sys
+this = sys.modules[__name__]  # Passed to create_test
+
+TC = Creator(this)
 
 
 # TODO:
 # - [x] Figure out issue with sender unable to connect
-# - [ ] Add test code generator
+# - [x] Add test code generator
 # - [ ] add: AddSource operation (and logic to validate)
 # - [x] background crash detector with notifier-short-circuit capability
 #       to eliminate timeout periods when a test fails due to crashed workers
 # - [x] Clean out dev debug logs
 
-#############
+
+
+CMD_PONY = 'multi_partition_detector --depth 1 --gen-source'
+CMD_PYTHON = 'machida --application-module multi_partition_detector --depth 1 --gen-source'
+
+APIS = {'pony': CMD_PONY, 'python': CMD_PYTHON}
+
+
+##############
 # Test spec(s)
+##############
+
+RESILIENCE_TEST_NAME_FMT = 'test_resilience_{api}_{ops}'
+
+##################
+# Resilience Tests
+##################
+
+RESILIENCE_SEQS = [
+    # wait, grow1, wait, shrink1, wait, crash2, wait, recover
+    [Wait(2), Grow(1), Wait(2), Shrink(1), Wait(2), Crash(2),
+     Wait(2), Recover(2), Wait(2)],
+
+    # crash1, recover1
+    [Wait(2), Crash(1), Wait(2), Recover(1), Wait(2)],
+
+    # crash2, recover2
+    [Wait(2), Crash(2), Wait(2), Recover(2), Wait(2)]]
+
+# Generate resilience test functions for each api, for each of the op seqs
+for api, cmd in APIS.items():
+    for ops in RESILIENCE_SEQS:
+        TC.create(RESILIENCE_TEST_NAME_FMT, api, cmd, ops,
+                  sources=0)
+
+
+
+print(globals())
+print()
+
 #############
-
-def test_auto0():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(1)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto1():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(1), Wait(2), Grow(4)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto2():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(1), Wait(2), Shrink(1)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto3():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(1), Wait(2), Shrink(4)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto4():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(4)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto5():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(4), Wait(2), Grow(1)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto6():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(4), Wait(2), Shrink(1)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto7():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Grow(4), Wait(2), Shrink(4)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto8():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(1)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto9():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(1), Wait(2), Shrink(4)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto10():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(1), Wait(2), Grow(1)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto11():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(1), Wait(2), Grow(4)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto12():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(4)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto13():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(4), Wait(2), Shrink(1)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto14():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(4), Wait(2), Grow(1)]
-    _test_resilience(command, ops, validate_output=True)
-
-def test_auto15():
-    command = 'multi_partition_detector --depth 1'
-    ops = [Shrink(4), Wait(2), Grow(4)]
-    _test_resilience(command, ops, validate_output=True)
-
-
-# some fixed tests:
-def test_grow1_shrink1_crash2_wait2_recover2():
-    command = 'multi_partition_detector --depth 1 --internal-source'
-    ops = [Wait(2), Grow(1), Wait(2), Shrink(1), Wait(2), Crash(2), Wait(2), Recover(2)]
-    _test_resilience(command, ops, validate_output=True, sources=0)
-
-
-def test_crash1_wait2_recover1():
-    command = 'multi_partition_detector --depth 1 --internal-source'
-    ops = [Wait(2), Crash(1), Wait(2), Recover(1)]
-    _test_resilience(command, ops, validate_output=True, sources=0)
-
-
-def test_crash2_wait2_recover2():
-    command = 'multi_partition_detector --depth 1 --internal-source'
-    ops = [Wait(2), Crash(2), Wait(2), Recover(2)]
-    _test_resilience(command, ops, validate_output=True, sources=0)
-
+# Fixed Tests
+#############
 
 # def test_grow1_wait2_shrink1_wait_2_times_ten():
-#     command = 'multi_partition_detector --depth 1 --internal-source'
+#     command = 'multi_partition_detector --depth 1 --gen-source'
 #     ops = [Wait(2), Grow(1), Wait(2), Shrink(1), Wait(2)]
 #     _test_resilience(command, ops, validate_output=True, cycles=10, sources=0)
 
