e35c3c6d2f0196a6ae2db7b19f3c21e7e9fac736
Nisan Haramati <hanisan@gmail.com>
2018-11-19 12:29:41 -0800

    Add debug prints

--- testing/correctness/apps/dummy_python/dummy.py
+++ testing/correctness/apps/dummy_python/dummy.py
@@ -16,6 +16,7 @@ import pickle
 import struct
 import wallaroo
 
+
 def application_setup(args):
     in_host, in_port = wallaroo.tcp_parse_input_addrs(args)[0]
     out_host, out_port = wallaroo.tcp_parse_output_addrs(args)[0]
@@ -26,11 +27,15 @@ def application_setup(args):
       .key_by(partition)
       .to(count_partitioned)
       .to_sink(wallaroo.TCPSinkConfig(out_host, out_port, encoder)))
-    return wallaroo.build_application("Dummy", pipeline)
+    ba = wallaroo.build_application("Dummy", pipeline)
+    print("application setuped!", ba)
+    return ba
+
 
 
 @wallaroo.key_extractor
 def partition(data):
+    print("partition({})".format(data))
     return str(hash(data))
 
 
@@ -39,8 +44,10 @@ class StateObject(object):
         self.val = 0
 
     def update(self, value):
-        self.val = value
-        return value
+        print("StateObject.update({})".format(value))
+        self.val = value.encode()
+        return self.val
+
 
 
 class PartitionedStateObject(object):
@@ -48,27 +55,34 @@ class PartitionedStateObject(object):
         self.val = 0
 
     def update(self, value):
-        self.val = value
-        return value
+        print("PartitionedStateObject.update({})".format(value))
+        self.val = value.encode()
+        return self.val
+
 
 
 @wallaroo.state_computation(name="Count State Updates", state=StateObject)
 def count(data, state):
+    print("count({},{})".format(data, state))
     res = state.update(data)
     return (res, True)
 
 
 @wallaroo.state_computation(name="Count Partitioned State Updates", state=PartitionedStateObject)
 def count_partitioned(data, state):
+    print("count_partitioned({},{})".format(data, state))
     res = state.update(data)
     return (res, True)
 
 
 @wallaroo.decoder(header_length=4, length_fmt=">I")
 def decoder(bs):
-    return bs
+    print("decoder({})".format(bs))
+    return bs.encode()
+
 
 
 @wallaroo.encoder
 def encoder(data):
-    return bytes(data)
+    print("encoder({})".format(data))
+    return data.encode()
