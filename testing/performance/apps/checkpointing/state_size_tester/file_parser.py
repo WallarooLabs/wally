b1408e4a0d9978196325293b3b31d1cbe2587d1e
Nisan Haramati <hanisan@gmail.com>
2019-04-17 17:51:11 -0700

    Update post-processing to use pandas
    - produce a single dataframe for the data
    - produce a dataframe for stats of latency and throughputs

--- testing/performance/apps/checkpointing/state_size_tester/file_parser.py
+++ testing/performance/apps/checkpointing/state_size_tester/file_parser.py
@@ -1,6 +1,10 @@
 from json import loads
 from math import floor
 import numpy as np
+import os
+
+
+PERCENTILES = [0, 25, 50, 75, 90, 95, 99.9, 99, 99.99, 100]
 
 
 def parse_output_line(line):
@@ -20,32 +24,29 @@ def load_output_file(path):
                 continue
     # convert time to offset from start
     # And convert array of latencies to percentiles:
-    percentiles = [0, 25, 50, 75, 90, 95, 99.9, 99, 99.99, 100]
     initial_keys = list(aggs.keys())
     t0 = min(initial_keys)
     for ik in initial_keys:
         arr = np.array(aggs.pop(ik))
         aggs[ik] = {'percentiles': [(p, np.percentile(arr, p))
-                                       for p in percentiles],
+                                       for p in PERCENTILES],
                        'count': len(arr)}
     return aggs
 
 
 def parse_checkpoints(data):
-    timestamps, delays, sizes, ids = [], [], [], []
-    t0 = data[0]['ts']
-    last_t = t0
-    last_id = list(data[0]['checkpoint_ids'].values())[0]
+    out = {}
     for rec in data:
-        timestamps.append(rec['ts'])
-        if list(rec['checkpoint_ids'].values())[0] > last_id:
-            delays.append(rec['ts'] - last_t)
-            last_id = list(rec['checkpoint_ids'].values())[0]
-        last_t = rec['ts']
-        sizes.append(list(rec['log_bytes'].values())[0])
-        ids.append(list(rec['checkpoint_ids'].values())[0])
-    return {'timestamps': timestamps, 'delays': delays,
-            'sizes': sizes, 'ids': ids}
+        # key on second, but record full precision timestamp (as fractional second)
+        key = int(floor(rec['ts']/1e9))
+        for fn in rec['log_bytes'].keys():
+            f_key = os.path.splitext(fn)[0]
+            f_data = out.setdefault(f_key, {})
+            r_data = f_data.setdefault(key, {})
+            r_data['ts'] = rec['ts']/1e9
+            r_data['size'] = rec['log_bytes'][fn]
+            r_data['id'] = rec['checkpoint_ids'][f_key + '.checkpoint_ids']
+    return out
 
 
 def load_checkpoints_file(path):
