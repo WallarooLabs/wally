1478c3ac63c268764a1ea5f624789643dec0c73b
Scott Lystig Fritchie <slfritchie@snookles.com>
2019-06-19 13:46:31 -0500

    Done with first draft: replace @printf behind-the-scenes
    We don't have many (any?) places where `printf(3)` calls are
    embedded in the C libraries that we're using (though I haven't
    checked all of the Python code yet, and it's possible that a
    Python function such as `print` might end up calling
    `printf(3)`?), so this flat-out replacement will get us a
    lot of mileage.
    
    The statement added to data_receiver.pony emits the following
    when run:
    
        1560980285.457195 SLF: Hello, world!

---
+++ lib/wallaroo_labs/logging/logging-lib.c
@@ -0,0 +1,347 @@
+/*
+
+Copyright (C) 2016-2019, Wallaroo Labs
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+#define ENV_BUF_SIZE  1024
+#define FMT_BUF_SIZE  1024
+#define MAX_SEVERITY  8
+#define MAX_CATEGORY  64
+
+static int _labels_initialized = 0;
+
+static char *_severity_labels[MAX_SEVERITY+1];
+static char *_category_labels[MAX_CATEGORY+1];
+
+static int _cat2sev_threshold[MAX_CATEGORY+1];
+
+/*****************************/
+/* Internal static functions */
+/*****************************/
+
+static int _w_now(char *dst, int dst_size)
+{
+  struct timeval tv;
+
+  gettimeofday(&tv, NULL);
+#ifdef  __APPLE__  
+  return snprintf(dst, dst_size, "%ld.%06d", tv.tv_sec, tv.tv_usec);
+#else
+  return snprintf(dst, dst_size, "%ld.%06lu", tv.tv_sec, tv.tv_usec);
+#endif
+}
+
+static int _w_vprintf(const char *fmt, va_list ap)
+{
+  char fmt2[FMT_BUF_SIZE];
+  int fmt2_len;
+  int ret;
+
+  fmt2_len = _w_now(fmt2, sizeof(fmt2));
+  if (strlen(fmt) > (sizeof(fmt2) - fmt2_len - 2 /* , & NUL */)) {
+    /* Our static buffer isn't big enough, so call vprintf() as is. */
+    ret = vprintf(fmt, ap);
+  } else {
+    fmt2[fmt2_len] = ',';
+    memcpy(fmt2 + fmt2_len + 1, fmt, strlen(fmt) + 1);
+    ret = vprintf(fmt2, ap);
+  }
+  va_end(ap);
+  return ret;
+}
+
+static void _w_initialize_labels()
+{
+  int i;
+  char buf[16];
+
+  for (i = 0; i < MAX_SEVERITY+1; i++) {
+    snprintf(buf, sizeof(buf), "sev-%d", i);
+    _severity_labels[i] = strdup(buf);
+  }
+  for (i = 0; i < MAX_CATEGORY+1; i++) {
+    snprintf(buf, sizeof(buf), "cat-%d", i);
+    _category_labels[i] = strdup(buf);
+    _cat2sev_threshold[i] = MAX_SEVERITY;
+  }
+  _labels_initialized = 1;
+}
+
+static int _w_l(unsigned char severity, unsigned char category,
+  const char *fmt, va_list ap)
+{
+  char fmt2[FMT_BUF_SIZE];
+
+  if (severity > _cat2sev_threshold[category]) {
+    return 0;
+  }
+  if (! _labels_initialized) {
+    _w_initialize_labels();
+  }
+
+  snprintf(fmt2, sizeof(fmt2), "%s,%s,%s\n",
+    _severity_labels[severity], _category_labels[category], fmt);
+  return _w_vprintf(fmt2, ap);
+}
+
+/********************/
+/* Public functions */
+/********************/
+
+int printf(const char *fmt, ...)
+{
+  va_list ap;
+
+  va_start(ap, fmt);
+  return _w_vprintf(fmt, ap);
+  // va_end() already done
+}
+
+/*
+** log_enabled(), is logging enabled for a (severity, category) tuple
+**
+** severity - numeric severity from 0 to MAX_SEVERITY
+** category - application category number from 0 to MAX_CATEGORY
+**
+** Return value: true if logging is enabled for this tuple,
+**               otherwise false
+*/
+
+unsigned char log_enabled(unsigned char severity, unsigned char category)
+{
+  if (severity > _cat2sev_threshold[category]) {
+    return 0;
+  }
+  return 1;
+}
+
+/*
+** ll_enabled(), is logging enabled for an unsigned short severity+category
+**
+** sev_cat: a mix of:
+**     severity - numeric severity, bits 8-15
+**     category - application category number, bits 0-7
+**
+** Return value: true if logging is enabled for this tuple,
+**               otherwise false
+*/
+
+unsigned char ll_enabled(unsigned short sev_cat)
+{
+  unsigned char severity = sev_cat & 0xFF;
+  unsigned char category = (sev_cat >> 8) & 0xFF;
+
+  if (severity > _cat2sev_threshold[category]) {
+    return 0;
+  }
+  return 1;
+}
+
+/*
+** l(), the severity + category + format + variable-args logging function.
+**
+** severity - numeric severity from 0 to MAX_SEVERITY
+** category - application category number from 0 to MAX_CATEGORY
+** fmt - snprintf(3)-style formatting string
+** 0 or or arguments - snprintf(3)-style arguments
+**
+** Return value: # of bytes written
+*/
+
+int l(unsigned char severity, unsigned char category, const char *fmt, ...)
+{
+  va_list ap;
+
+  if (severity > _cat2sev_threshold[category]) {
+    return 0;
+  }
+  if (! _labels_initialized) {
+    _w_initialize_labels();
+  }
+
+  va_start(ap, fmt);
+  return _w_l(severity, category, fmt, ap);
+}
+
+/*
+** ll(), the severity + category + format + variable-args logging function.
+**
+** sev_cat: a mix of:
+**     severity - numeric severity, bits 8-15
+**     category - application category number, bits 0-7
+** fmt - snprintf(3)-style formatting string
+** 0 or or arguments - snprintf(3)-style arguments
+**
+** Return value: # of bytes written
+*/
+
+int ll(unsigned short sev_cat, const char *fmt, ...)
+{
+  unsigned char severity = sev_cat & 0xFF;
+  unsigned char category = (sev_cat >> 8) & 0xFF;
+  va_list ap;
+
+  if (severity > _cat2sev_threshold[category]) {
+    return 0;
+  }
+  if (! _labels_initialized) {
+    _w_initialize_labels();
+  }
+
+  va_start(ap, fmt);
+  return _w_l(severity, category, fmt, ap);
+}
+
+/*
+** w_set_severity_label(), set the formatted label for a severity number
+**
+** severity - numeric severity from 0 to MAX_SEVERITY
+*/
+
+void w_set_severity_label(unsigned char severity, char *label)
+{
+  if (! _labels_initialized) {
+    _w_initialize_labels();
+  }
+  if (severity < MAX_SEVERITY+1) {
+    _severity_labels[severity] = strdup(label);
+  }
+}
+
+/*
+** w_set_category_label(), set the formatted label for a category number
+**
+** category - application category number from 0 to MAX_CATEGORY
+*/
+
+void w_set_category_label(unsigned char category, char *label)
+{
+  if (! _labels_initialized) {
+    _w_initialize_labels();
+  }
+  if (category < MAX_CATEGORY+1) {
+    _category_labels[category] = strdup(label);
+  }
+}
+
+/*
+** w_set_severity_threshold(), set the severity threshold for all categories
+**
+** severity - numeric severity from 0 to MAX_SEVERITY
+*/
+
+void w_set_severity_threshold(unsigned char severity)
+{
+  int i;
+
+  if (severity < MAX_SEVERITY+1) {
+    for (i = 0; i < MAX_CATEGORY+1; i++) {
+      _cat2sev_threshold[i] = severity;
+    }
+  }
+}
+
+/*
+** w_set_severity_cat_threshold(), set the severity threshold for a category
+**
+** severity - numeric severity from 0 to MAX_SEVERITY
+** category - application category number from 0 to MAX_CATEGORY
+*/
+
+void w_set_severity_cat_threshold(unsigned char severity, unsigned char category)
+{
+  int i;
+
+  if (severity < MAX_SEVERITY+1 && category < MAX_CATEGORY+1) {
+    _cat2sev_threshold[category] = severity;
+  }
+}
+
+/*
+** w_process_category_overrides, process any env var overrides
+**
+** If the environment variable WALLAROO_THRESHOLDS exists, then
+** parse that string and apply a set of overrides to the
+** _cat2sev_thresholds.
+**
+** The string should contain a list of zero or more
+** category.severity pairs, e.g, "22.5" for category 23 and
+** severity 5 (which corresponds to Log.notice()).  The string
+** "*" can be used to specify all categories.
+**
+** Items in the list are separated by commas and are processed
+** in order.  The "*" for all categories, if used, probably ought
+** to be first in the list.  For example, "*.1,22.5,3.7,4.0".
+*/
+
+void w_process_category_overrides()
+{
+  char *env = getenv("WALLAROO_THRESHOLDS");
+  char buf[ENV_BUF_SIZE];
+  char *p = buf, *start, *saveptr, *dot;
+  unsigned char severity, category;
+  int i;
+
+  if (env != NULL) {
+    if (strlen(env) < sizeof(buf) - 1) {
+      strcpy(buf, env);
+
+      for (start = strtok_r(buf, ",", &saveptr);
+           start != NULL;
+           start = strtok_r(NULL, ",", &saveptr)) {
+        if ((dot = strchr(start, '.')) != NULL) {
+          *dot = '\0';
+          severity = atoi(dot + 1);
+          if (*start == '*') {
+            w_set_severity_threshold(severity);
+          } else if ((category = atoi(start)) > 0) {
+            w_set_severity_cat_threshold(severity, category);
+          }
+        }
+      }
+    }
+  }
+}
+
+#ifdef MAIN
+#include <assert.h>
+int main(void)
+{
+  char *fmt1 = "Hello, world (with timestamp)!\n";
+  char *fmt2 = "Hello, world (too big, no timestamp) Hello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, world!\n";
+
+  assert(strlen(fmt1) < FMT_BUF_SIZE);
+  printf("Hello, world (with timestamp)!\n");
+  assert(strlen(fmt2) > FMT_BUF_SIZE);
+  printf("Hello, world (too big, no timestamp) Hello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, worldHello, world!\n");
+}
+#endif
---
+++ lib/wallaroo_labs/logging/wallaroo-logging.pony
@@ -0,0 +1,117 @@
+/*
+
+Copyright 2019 The Wallaroo Authors.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ implied. See the License for the specific language governing
+ permissions and limitations under the License.
+
+*/
+
+// Tell ponyc that we're going to use the external C functions
+// in the libwallaroo-logging.a library.
+use "lib:wallaroo-logging"
+
+// C FFI prototypes for cut-and-paste into your Pony source, as needed
+use @printf[I32](fmt: Pointer[U8] tag, ...)
+use @log_enabled[Bool](severity: LogSeverity, category: LogCategory)
+use @ll_enabled[Bool](sev_cat: U16)
+use @l[I32](severity: LogSeverity, category: LogCategory, fmt: Pointer[U8] tag, ...)
+use @ll[I32](sev_cat: U16, fmt: Pointer[U8] tag, ...)
+use @w_set_severity_label[None](severity: LogSeverity, label: Pointer[U8] tag)
+use @w_set_category_label[None](category: LogCategory, label: Pointer[U8] tag)
+use @w_set_severity_threshold[None](severity: LogSeverity)
+use @w_set_severity_cat_threshold[None](severity: LogSeverity, category: LogCategory)
+use @w_process_category_overrides[None]()
+
+type LogCategory is U8
+type LogSeverity is U8
+
+primitive Log
+  // severity levels
+  fun no_sev(): LogSeverity   => LogSeverity(0)
+  fun emerg(): LogSeverity  => LogSeverity(1)
+  fun alert(): LogSeverity  => LogSeverity(2)
+  fun crit(): LogSeverity   => LogSeverity(3)
+  fun err(): LogSeverity    => LogSeverity(4)
+  fun warn(): LogSeverity   => LogSeverity(5)
+  fun notice(): LogSeverity => LogSeverity(6)
+  fun info(): LogSeverity   => LogSeverity(7)
+  fun debug(): LogSeverity  => LogSeverity(8)
+  fun max_severity(): LogSeverity => debug()
+  fun default_severity(): LogSeverity => info()
+
+  // categories
+  fun no_cat(): LogCategory             => LogCategory(0)
+  fun checkpoint(): LogCategory       => LogCategory(1)
+  fun source_migration(): LogCategory => LogCategory(2)
+  fun twopc(): LogCategory            => LogCategory(3)
+  fun dos_client(): LogCategory       => LogCategory(4)
+  fun tcp_sink(): LogCategory         => LogCategory(5)
+  fun conn_sink(): LogCategory        => LogCategory(6)
+  fun tcp_source(): LogCategory       => LogCategory(7)
+  fun conn_source(): LogCategory      => LogCategory(8)
+
+  fun severity_map(): Array[(LogSeverity, String)] =>
+    [ // BEGIN severity_map
+      (no_sev(), "NONE")
+      (emerg(),  "EMERGENCY")
+      (alert(),  "ALERT")
+      (crit(),   "CRITICAL")
+      (err(),    "ERROR")
+      (warn(),   "WARNING")
+      (notice(), "NOTICE")
+      (info(),   "INFO")
+      (debug(),  "DEBUG")
+    ] // END severity_map
+
+  fun category_map(): Array[(LogSeverity, LogCategory, String)] =>
+    [ // BEGIN category_map
+      (default_severity(), no_cat(),           "none")
+      (default_severity(), checkpoint(),       "checkpoint")
+      (default_severity(), source_migration(), "source-migration")
+      (default_severity(), twopc(),            "2PC")
+      (default_severity(), dos_client(),       "DOSClient")
+      (default_severity(), tcp_sink(),         "TCPSink")
+      (default_severity(), conn_sink(),        "ConnectorSink")
+      (default_severity(), tcp_source(),         "TCPSource")
+      (default_severity(), conn_source(),        "ConnectorSource")
+    ] // END category_map
+
+  fun set_defaults() =>
+    set_severity_labels()
+    set_category_labels()
+    set_thresholds()
+
+  fun set_severity_labels() =>
+    for (severity, label) in severity_map().values() do
+      @w_set_severity_label(severity, label.cstring())
+    end
+
+  fun set_category_labels() =>
+    for (severity, category, label) in category_map().values() do
+      @w_set_category_label(category, label.cstring())
+    end
+
+  fun set_thresholds(do_defaults: Bool = true, do_overrides: Bool = true) =>
+    if do_defaults then
+      @w_set_severity_threshold(default_severity())
+      for (severity, category, label) in category_map().values() do
+        @w_set_severity_cat_threshold(severity, category)
+      end
+    end
+
+    if do_overrides then
+      @w_process_category_overrides[None]()
+    end
+
+  fun make_sev_cat(severity: LogSeverity, category: LogCategory): U16 =>
+    severity.u16().shl(8) + category.u16()
