85c3ade34c100ac25a0105caefc6f3f26bba1841
John Mumm <jtfmumm@gmail.com>
2019-10-11 08:32:57 +0200

    Update go.sh

--- testing/correctness/scripts/effectively-once/go.sh
+++ testing/correctness/scripts/effectively-once/go.sh
@@ -4,4 +4,4 @@ dd if=testing/data/market_spread/nbbo/r3k-symbols_nbbo-fixish.msg bs=1000000 cou
 cd testing/correctness/scripts/effectively-once
 make -C ../../../.. PONYCFLAGS="--verbose=1 -d -Dresilience -Dtrace -Dcheckpoint_trace -Didentify_routing_ids" build-examples-pony-passthrough build-testing-tools-external_sender     build-utils-cluster_shrinker
 
-./master-crasher.sh 3 crash1 crash2 crash-sink
+#./master-crasher.sh 3 crash1 crash2 crash-sink
