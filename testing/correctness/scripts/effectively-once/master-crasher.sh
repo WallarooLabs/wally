c90a9d18545c6998b11d94f42d2768c965b5bb0e
Scott Lystig Fritchie <slfritchie@snookles.com>
2019-12-13 14:26:24 -0600

    Add multi-key input and output checking to master-crasher.sh
    See the sample-env-vars.sh* files for suggested default values:
    10 independent keys, A-J, enumerated by the MULTIPLE_KEYS_LIST
    environment variable.  The sanity loop of master-crasher.sh will
    concatenate all sink outputs into a single ordered file, then
    extract only a single key, then verify that the output order for
    that key is exactly equal to a strict prefix of the input file.
    
    Also:
    
    * Add '.slow' suffix option for 'crashN' arguments, which will crash
      the Nth worker only every 2 minutes (approximately).  This is useful
      if an external script/proc is attempting to manage disk space by
      rotating out old log data.
    
    * Add run_custom_20191210a arg for convenience in testing the
      scenario 'crash0 crash-sink' in a more repeatable way.
    
    * Various bug & typo fixes
    
    * Fix error handling bug in .../aloc_sink/Makefile

--- testing/correctness/scripts/effectively-once/master-crasher.sh
+++ testing/correctness/scripts/effectively-once/master-crasher.sh
@@ -109,20 +109,27 @@ start_all_workers () {
     poll_ready -a -v -w 5 || exit 1
 }
 
-start_sender () {
-    rm -f $SENDER_OUTFILE
-    while [ 1 ]; do
-        if [ "$WALLAROO_TCP_SOURCE_SINK" = "true" ]; then
-            $WALLAROO_TOP/testing/tools/fixed_length_message_blaster/fixed_length_message_blaster \
-            --host localhost:$WALLAROO_IN_BASE --file /tmp/input-file.txt \
-            --msg-size 53 --batch-size 6 \
-            --msec-interval 5 --report-interval 1000000 --ponythreads=1 \
-            >> $SENDER_OUTFILE 2>&1
-        else
-            env ERROR_9_SHOULD_EXIT=true $WALLAROO_TOP/testing/correctness/scripts/effectively-once/at_least_once_line_file_feed /tmp/input-file.txt 66000 >> $SENDER_OUTFILE 2>&1
-        fi
-        sleep 0.1
+start_senders () {
+    rm -f $SENDER_OUTFILE.*
+    if [ -z "$MULTIPLE_KEYS_LIST" ]; then
+        echo ERROR: The variable \$MULTIPLE_KEYS_LIST is empty, exiting!
+        exit 1
+    fi
+    for i in $MULTIPLE_KEYS_LIST; do
+        while [ 1 ]; do
+            if [ "$WALLAROO_TCP_SOURCE_SINK" = "true" ]; then
+                $WALLAROO_TOP/testing/tools/fixed_length_message_blaster/fixed_length_message_blaster \
+                --host localhost:$WALLAROO_IN_BASE --file /tmp/input-file.$i.txt \
+                --msg-size 53 --batch-size 6 \
+                --msec-interval 5 --report-interval 1000000 --ponythreads=1 \
+                >> $SENDER_OUTFILE.$i 2>&1
+            else
+                env ERROR_9_SHOULD_EXIT=true $WALLAROO_TOP/testing/correctness/scripts/effectively-once/at_least_once_line_file_feed /tmp/input-file.$i.txt 66000 >> $SENDER_OUTFILE.$i 2>&1
+            fi
+            sleep 0.1
+        done &
     done
+    wait
 }
 
 stop_sender () {
@@ -187,20 +194,35 @@ run_crash_sink_loop () {
 
 run_crash_worker_loop () {
     worker="$1"
+    suffix="$2"
+
     if [ -z "$worker" ]; then
         echo ERROR: $0: worker number not given
         print_duration
         exit 1
     fi
     sleep 2 # Don't start crashing until checkpoint #1 is complete.
+    counter=0
     while [ 1 ]; do
+        counter=`expr $counter + 1`
         if [ -f $STATUS_CRASH_WORKER ]; then
-            sleep `random_float 4.5 0`
+            case "$suffix" in
+                *slow*)
+                    ## Space apart the crashes very roughly 120+fudge seconds apart
+                    if [ $counter -lt 120 ]; then
+                        sleep 1
+                        sleep `random_float 0.2`
+                        continue
+                    fi
+                    counter=0
+                ;;
+            esac
+            sleep `random_float 4.5`
             /bin/echo -n "c$worker"
             crash_out=`crash_worker $worker`
             mv /tmp/wallaroo.$worker /tmp/wallaroo.$worker.`date +%s` && gzip /tmp/wallaroo.$worker.`date +%s` > /dev/null 2>&1 &
             if [ -z "$crash_out" ]; then
-                sleep `random_float 2.5 0`
+                sleep `random_float 2.5`
                 if [ $worker -eq 0 ]; then
                     start_initializer
                 else
@@ -254,16 +276,34 @@ run_sanity_loop () {
             echo SANITY
             break
         fi
-        ./1-to-1-passthrough-verify.sh /tmp/input-file.txt > $outfile 2>&1
-        if [ $? -ne 0 ]; then
-            head $outfile
+        multi_key_tmp_file=/tmp/concat-multikey-tmp-file
+        rm -f $multi_key_tmp_file
+        errors=0
+        for i in $MULTIPLE_KEYS_LIST; do
+            ./1-to-1-passthrough-verify.sh $i /tmp/input-file.$i.txt $multi_key_tmp_file > $outfile 2>&1
+            if [ $? -ne 0 ]; then
+                echo ""
+                head $outfile
+                rm $outfile
+                echo SANITY2
+                errors=1
+            fi
             rm $outfile
-            echo BREAK2
-            break
+        done
+        if [ $errors -ne 0 ]; then
+            print_duration
+            pause_the_world
         fi
-        rm $outfile
-        res=`logtail $SENDER_OUTFILE | egrep 'MultiSourceConnector closed Stream.*, point_of_ref=12368928, is_open=False'`
-        if [ ! -z "$res" ]; then
+        total=0
+        found=0
+        for i in $MULTIPLE_KEYS_LIST; do
+            total=`expr $total + 1`
+            res=`logtail $SENDER_OUTFILE.$i | egrep 'MultiSourceConnector closed Stream.*, point_of_ref=12368928, is_open=False'`
+            if [ ! -z "$res" ]; then
+                found=`expr $found + 1`
+            fi
+        done
+        if [ $found -eq $total ]; then
             echo "SANITY LOOP SUCCESS: EOF found!"
             echo "SANITY LOOP SUCCESS: EOF found!" >> /tmp/res
             print_duration
@@ -567,12 +607,49 @@ run_custom_crashsink_crash0_overlap () {
     fi
 }
 
+run_custom_20191210a () {
+    ## Assume that we are run with `./master-crasher.sh 2 run_custom_20191210a`
+
+    while [ 1 ]; do
+        (
+            sleep 4.9
+            /bin/echo -n c0
+            crash_worker 0
+            mv /tmp/wallaroo.0 /tmp/wallaroo.0.`date +%s` && gzip /tmp/wallaroo.0.`date +%s` > /dev/null 2>&1 &
+            sleep 0.9
+            /bin/echo -n r0
+            start_initializer
+        ) &
+        (
+            sleep 3.7
+            /bin/echo -n cS
+            crash_sink
+            sleep 1.0
+            /bin/echo -n rS
+            start_sink
+        ) &
+        wait
+        /bin/echo -n echo both-done.
+        poll_out=`poll_ready -w 4 2>&1`
+        if [ $? -ne 0 -o ! -z "$poll_out" ]; then
+            echo "BUMMER A: pause the world: $poll_out"
+            poll_out=`poll_ready -w 4 2>&1`
+            if [ $? -ne 0 -o ! -z "$poll_out" ]; then
+                echo "BUMMER B: pause the world: $poll_out"
+                pause_the_world
+                break
+            fi
+        fi
+        sleep 10
+    done
+}
+
 touch $STATUS_CRASH_WORKER
 touch $STATUS_CRASH_SINK
 reset
 start_sink ; sleep 1
 start_all_workers
-start_sender &
+start_senders &
 
 run_sanity=true
 for arg in $*; do
@@ -586,8 +663,14 @@ for arg in $*; do
             run_sanity=false
             ;;
         crash[0-9]*)
-            worker=`echo $arg | sed 's/crash//'`
-            cmd="run_crash_worker_loop $worker"
+            worker=`echo $arg | sed -e 's/crash//' -e 's/\..*//'`
+            suffix=""
+            case $arg in
+                *.*)
+                    suffix=`echo $arg | sed 's/crash[0-9]*\.//'`
+                ;;
+            esac
+            cmd="run_crash_worker_loop $worker $suffix"
             echo RUN: $cmd
             $cmd &
             ;;
